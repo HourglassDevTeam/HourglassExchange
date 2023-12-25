eb8a7c13c27281bf311c12d594512c17a8296fe4
arthur@ciuyat.com <arthur@ciuyat.com>
2023-12-25 18:59:53 +0800

    updated

--- src/error.rs
+++ src/error.rs
@@ -5,18 +5,18 @@ use thiserror::Error;
 
 #[derive(Error, PartialEq, Eq, PartialOrd, Debug, Clone, Deserialize, Serialize)]
 pub enum ExecutionError {
-    #[error("failed to build component due to missing attributes: {0}")]
+    #[error("[CerebroBroker] : 【cerebro_brokerfailed to build component due to missing attributes: {0}")]
     BuilderIncomplete(String),
 
-    #[error("SimulatedExchange error: {0}")]
+    #[error("[CerebroBroker] : SimulatedExchange error: {0}")]
     Simulated(String),
 
-    #[error("Balance for symbol {0} insufficient to open order")]
+    #[error("[CerebroBroker] : Balance for symbol {0} insufficient to open order")]
     InsufficientBalance(Symbol),
 
-    #[error("failed to find Order with ClientOrderId: {0}")]
+    #[error("[CerebroBroker] : failed to find Order with ClientOrderId: {0}")]
     OrderNotFound(ClientOrderId),
 
-    #[error("failed to open Order due to unsupported OrderKind: {0}")]
+    #[error("[CerebroBroker] : failed to open Order due to unsupported OrderKind: {0}")]
     UnsupportedOrderKind(OrderKind),
 }
