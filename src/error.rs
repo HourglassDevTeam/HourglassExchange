1a250bde49e8991f951a0f708d8ed1b6df7db0fe
arthur@ciuyat.com <arthur@ciuyat.com>
2023-12-25 23:10:00 +0800

    updated

--- src/error.rs
+++ src/error.rs
@@ -4,20 +4,26 @@ use thiserror::Error;
 
 use crate::model::{ClientOrderId, order::OrderKind};
 
+/// 表示执行过程中可能遇到的错误。
 #[derive(Error, PartialEq, Eq, PartialOrd, Debug, Clone, Deserialize, Serialize)]
 pub enum ExecutionError {
-    #[error("[CerebroBroker] : Failed to build component due to missing attributes: {0}")]
+    /// 由于缺少属性，无法构建组件。
+    #[error("[CerebroBroker] : 由于缺少属性，无法构建组件: {0}")]
     BuilderIncomplete(String),
 
-    #[error("[CerebroBroker] : SimulatedExchange error: {0}")]
+    /// 模拟交易所出错。
+    #[error("[CerebroBroker] : 模拟交易所错误: {0}")]
     Simulated(String),
 
-    #[error("[CerebroBroker] : Balance for symbol {0} insufficient to open order")]
+    /// 余额不足，无法开设订单。
+    #[error("[CerebroBroker] : 符号{0}的余额不足，无法开设订单")]
     InsufficientBalance(Symbol),
 
-    #[error("[CerebroBroker] : failed to find Order with ClientOrderId: {0}")]
+    /// 找不到具有特定客户端订单ID的订单。
+    #[error("[CerebroBroker] : 未能找到具有客户端订单ID的订单: {0}")]
     OrderNotFound(ClientOrderId),
 
-    #[error("[CerebroBroker] : failed to open Order due to unsupported OrderKind: {0}")]
+    /// 由于不支持的订单类型，无法开设订单。
+    #[error("[CerebroBroker] : 由于不支持的订单类型，无法开设订单: {0}")]
     UnsupportedOrderKind(OrderKind),
 }
