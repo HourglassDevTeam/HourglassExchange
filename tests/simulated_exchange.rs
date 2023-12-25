7fea257b27e08f1098bd9f3ce05a52905968eb45
arthur@ciuyat.com <arthur@ciuyat.com>
2023-12-25 22:52:27 +0800

    updated

--- tests/simulated_exchange.rs
+++ tests/simulated_exchange.rs
@@ -784,13 +784,14 @@ async fn test_9_open_2x_limit_sell_orders(
     }
 }
 
-
+// 10. 发送一个完全匹配1x卖单的MarketEvent（交易），并部分匹配另一个（交易）。检查是否发送了两次匹配的余额和交易的AccountEvents。
 // 10. Send MarketEvent that fully matches 1x sell Order (trade), and partially matches the another
 //    (trade). Check AccountEvents for balances and trades of both matches are sent.
 async fn test_10_send_market_event_that_full_and_partial_matches_orders(
     event_simulated_tx: &mut mpsc::UnboundedSender<SimulatedEvent>,
     event_account_rx: &mut mpsc::UnboundedReceiver<AccountEvent>,
 ) {
+    // 发送一个完全匹配一个订单且部分匹配另一个订单的MarketEvent
     // Send MarketEvent that fully matches one order and partially matches another
     event_simulated_tx
         .send(SimulatedEvent::MarketTrade((
@@ -806,21 +807,26 @@ async fn test_10_send_market_event_that_full_and_partial_matches_orders(
 
     tokio::time::sleep(latency_50ms()).await;
 
+    // a) 第一个完全匹配的成交：
     // a) First full match fill:
     let first_full_fill_fees = (500.0 * 1.0) * fees_50_percent();
+    // 检查与交易相关的基础和报价货币的AccountEvent Balances
     // Check AccountEvent Balances for base & quote currencies related to the trade
     match event_account_rx.try_recv() {
         Ok(AccountEvent {
-            kind: AccountEventKind::Balances(balances),
-            ..
-        }) => {
+               kind: AccountEventKind::Balances(balances),
+               ..
+           }) => {
+            // 基础和报价SymbolBalances应该更新
             // Base & Quote SymbolBalances should be updated
             assert_eq!(balances.len(), 2);
 
+            // 首先检查基础余额：预期btc Balance { total: 10.5 - 1.0, available: 8.5 }
             // Base Balance first: expected btc Balance { total: 10.5 - 1.0, available: 8.5 }
             let expected_btc = SymbolBalance::new("btc", Balance::new(10.5 - 1.0, 8.5));
             assert_eq!(balances[0], expected_btc);
 
+            // 然后检查报价余额：预期的usdt增加 = (500 * 1.0) - (500 * 1.0 * 0.5) = 500 - 250 = 250
             // Quote Balance second:
             // Expected usdt increase = (500 * 1.0) - (500 * 1.0 * 0.5) = 500 - 250 = 250
             // expected usdt Balance { total: 9_800 + 250, available: 9_700 + 250 }
@@ -835,11 +841,13 @@ async fn test_10_send_market_event_that_full_and_partial_matches_orders(
         }
     }
 
+    // 检查匹配MarketEvent的AccountEvent Trade
+    // Check AccountEvent Trade for order matching MarketEvent
     match event_account_rx.try_recv() {
         Ok(AccountEvent {
-            kind: AccountEventKind::Trade(trade),
-            ..
-        }) => {
+               kind: AccountEventKind::Trade(trade),
+               ..
+           }) => {
             let expected = Trade {
                 id: TradeId(2.to_string()),
                 order_id: OrderId(4.to_string()),
@@ -859,24 +867,27 @@ async fn test_10_send_market_event_that_full_and_partial_matches_orders(
         }
     }
 
+    // b) 第二个部分匹配的成交
     // b) Second partial match fill
     let second_partial_fill_fees = (1000.0 * 0.5) * fees_50_percent();
 
+    // 检查与交易相关的基础和报价货币的AccountEvent Balances
     // Check AccountEvent Balances for base & quote currencies related to the trade
     match event_account_rx.try_recv() {
         Ok(AccountEvent {
-            kind: AccountEventKind::Balances(balances),
-            ..
-        }) => {
+               kind: AccountEventKind::Balances(balances),
+               ..
+           }) => {
+            // 基础和报价SymbolBalances应该更新
             // Base & Quote SymbolBalances should be updated
             assert_eq!(balances.len(), 2);
 
-            // btc { total: 9.0, available: 8.5 } 0.5 left in partially filled trade
-
+            // 首先检查基础余额：预期btc Balance { total: 9.5 - 0.5, available: 8.5 }
             // Base Balance first: expected btc Balance { total: 9.5 - 0.5, available: 8.5 }
             let expected_btc = SymbolBalance::new("btc", Balance::new(9.5 - 0.5, 8.5));
             assert_eq!(balances[0], expected_btc);
 
+            // 然后检查报价余额：预期的usdt增加 = (1000 * 0.5) - (1000 * 0.5 * 0.5) = 500 - 250 = 250
             // Quote Balance second:
             // Expected usdt increase = (1000 * 0.5) - (1000 * 0.5 * 0.5) = 500 - 250 = 250
             // expected usdt Balance { total: 10_050 + 250, available: 9_950 + 250 }
@@ -892,11 +903,13 @@ async fn test_10_send_market_event_that_full_and_partial_matches_orders(
         }
     }
 
+    // 检查匹配MarketEvent的AccountEvent Trade
+    // Check AccountEvent Trade for order matching MarketEvent
     match event_account_rx.try_recv() {
         Ok(AccountEvent {
-            kind: AccountEventKind::Trade(trade),
-            ..
-        }) => {
+               kind: AccountEventKind::Trade(trade),
+               ..
+           }) => {
             let expected = Trade {
                 id: TradeId(3.to_string()),
                 order_id: OrderId(5.to_string()),
@@ -916,6 +929,7 @@ async fn test_10_send_market_event_that_full_and_partial_matches_orders(
         }
     }
 
+    // 检查是否没有更多的AccountEvents生成
     // Check no more AccountEvents generated
     match event_account_rx.try_recv() {
         Err(mpsc::error::TryRecvError::Empty) => {}
@@ -927,7 +941,7 @@ async fn test_10_send_market_event_that_full_and_partial_matches_orders(
         }
     }
 }
-
+// 11. 取消所有打开的订单。包括部分填充的卖单和未填充的买单。检查是否发送了订单取消和余额的AccountEvents。
 // 11. Cancel all open orders. Includes a partially filled sell order, and non-filled buy order.
 //     Check AccountEvents for orders cancelled and balances are sent.
 async fn test_11_cancel_all_orders(
@@ -936,10 +950,13 @@ async fn test_11_cancel_all_orders(
     test_9_ids_2: Ids,
     event_account_rx: &mut mpsc::UnboundedReceiver<AccountEvent>,
 ) {
+    // 取消所有订单
     let cancelled = client.cancel_orders_all().await.unwrap();
 
+    // 预期的取消订单
     let expected_cancelled = vec![
         order_cancelled(
+            // 先取消买单
             // Bids are cancelled first
             Instrument::from(("btc", "usdt", InstrumentKind::Perpetual)),
             test_6_ids_1.cid,
@@ -947,6 +964,7 @@ async fn test_11_cancel_all_orders(
             test_6_ids_1.id,
         ),
         order_cancelled(
+            // 然后取消卖单
             // Asks are cancelled second
             Instrument::from(("btc", "usdt", InstrumentKind::Perpetual)),
             test_9_ids_2.cid,
@@ -955,16 +973,18 @@ async fn test_11_cancel_all_orders(
         ),
     ];
 
+    // 确认订单已被取消
     assert_eq!(cancelled.len(), 2);
     assert_eq!(cancelled[0].clone(), expected_cancelled[0]);
     assert_eq!(cancelled[1].clone(), expected_cancelled[1]);
 
+    // 检查买卖单的AccountEvent Order是否已被取消
     // Check AccountEvent Order cancelled for both the bid & ask
     match event_account_rx.try_recv() {
         Ok(AccountEvent {
-            kind: AccountEventKind::OrdersCancelled(cancelled),
-            ..
-        }) => {
+               kind: AccountEventKind::OrdersCancelled(cancelled),
+               ..
+           }) => {
             assert_eq!(cancelled.len(), 2);
             assert_eq!(cancelled[0].clone(), expected_cancelled[0]);
             assert_eq!(cancelled[1].clone(), expected_cancelled[1]);
@@ -977,25 +997,29 @@ async fn test_11_cancel_all_orders(
         }
     }
 
+    // 检查买卖单取消后的AccountEvent Balances
     // Check AccountEvent Balances for cancelled bid and ask orders
     match event_account_rx.try_recv() {
         Ok(AccountEvent {
-            kind: AccountEventKind::Balances(balances),
-            ..
-        }) => {
+               kind: AccountEventKind::Balances(balances),
+               ..
+           }) => {
+            // 买单报价和卖单基础的SymbolBalances应更新
             // SymbolBalances for Bid order quote, & ask order base should be updated
             assert_eq!(balances.len(), 2);
 
-            // Bids are cancelled first, so balance is updated first
+            // 先取消买单，因此首先更新余额
             // test_6_order_cid_1, Side::Buy, price=100.0, quantity=1.0
-            // Therefore, usdt Balance { total: 10_300, available: 10_200 + (100 * 1)
+            // 因此，usdt Balance { total: 10_300, available: 10_200 + (100 * 1)
+            // Bids are cancelled first, so balance is updated first
             let expected_usdt =
                 SymbolBalance::new("usdt", Balance::new(10_300.0, 10_200.0 + 100.0));
             assert_eq!(balances[0], expected_usdt);
 
-            // Asks are cancelled second, so balance is updated first
+            // 然后取消卖单，因此随后更新余额
             // test_9_order_cid_2, Side::Sell, price=1000.0, quantity=1.0, filled=0.5
-            // Therefore, btc Balance { total: 9.0, available: 8.5 + 0.5 }
+            // 因此，btc Balance { total: 9.0, available: 8.5 + 0.5 }
+            // Asks are cancelled second, so balance is updated second
             let expected_btc = SymbolBalance::new("btc", Balance::new(9.0, 8.5 + 0.5));
             assert_eq!(balances[1], expected_btc);
         }
@@ -1007,6 +1031,7 @@ async fn test_11_cancel_all_orders(
         }
     }
 
+    // 检查是否没有更多的AccountEvents生成
     // Check no more AccountEvents generated
     match event_account_rx.try_recv() {
         Err(mpsc::error::TryRecvError::Empty) => {}
@@ -1019,19 +1044,23 @@ async fn test_11_cancel_all_orders(
     }
 }
 
-// 12. Fetch open orders (now that we've called cancel_all) and check it is empty
+// 12. 获取打开的订单（现在我们已经调用了cancel_all）并检查是否为空
+// 12. Fetch open orders (now that we've called cancel_all) and check it is empty.
 async fn test_12_fetch_open_orders_and_check_empty(client: &SimulatedExecution) {
     let open_orders = client.fetch_orders_open().await.unwrap();
+    // 确认打开的订单列表为空
     assert!(open_orders.is_empty());
 }
 
-// 13. Fail to open limit buy order with insufficient funds
+// 13. 由于资金不足，未能打开限价买单
+// 13. Fail to open limit buy order with insufficient funds.
 async fn test_13_fail_to_open_one_of_two_limits_with_insufficient_funds(
     client: &SimulatedExecution,
     test_13_ids_1: Ids,
     test_13_ids_2: Ids,
     event_account_rx: &mut mpsc::UnboundedReceiver<AccountEvent>,
 ) {
+    // 尝试打开两个限价订单，其中一个由于资金不足而失败
     let opened_orders = client
         .open_orders(vec![
             order_request_limit(
@@ -1051,7 +1080,9 @@ async fn test_13_fail_to_open_one_of_two_limits_with_insufficient_funds(
         ])
         .await;
 
+    // 预期第一个订单由于资金不足而失败
     let expected_order_new_1 = Err(ExecutionError::InsufficientBalance(Symbol::from("usdt")));
+    // 预期第二个订单成功创建
     let expected_order_new_2 = open_order(
         Instrument::from(("btc", "usdt", InstrumentKind::Perpetual)),
         test_13_ids_2.cid,
@@ -1062,19 +1093,20 @@ async fn test_13_fail_to_open_one_of_two_limits_with_insufficient_funds(
         0.0,
     );
 
+    // 确认订单创建结果
     assert_eq!(opened_orders.len(), 2);
     assert_eq!(opened_orders[0].clone(), expected_order_new_1);
     assert_eq!(opened_orders[1].clone().unwrap(), expected_order_new_2);
 
-    // Note: First order failed to due usdt InsufficientBalance, so don't expect any AccountEvents
+    // 第一个订单由于usdt资金不足失败，因此不期待任何AccountEvents
 
-    // Check AccountEvent Balance for second order - quote currency has available balance decrease
+    // 检查第二个订单的AccountEvent Balance - 基础货币的可用余额减少
     match event_account_rx.try_recv() {
         Ok(AccountEvent {
-            kind: AccountEventKind::Balance(btc_balance),
-            ..
-        }) => {
-            // Expected btc Balance.available = 9.0 - 1.0
+               kind: AccountEventKind::Balance(btc_balance),
+               ..
+           }) => {
+            // 预期btc Balance.available = 9.0 - 1.0
             let expected = SymbolBalance::new("btc", Balance::new(9.0, 9.0 - 1.0));
             assert_eq!(btc_balance, expected);
         }
@@ -1086,12 +1118,12 @@ async fn test_13_fail_to_open_one_of_two_limits_with_insufficient_funds(
         }
     }
 
-    // Check AccountEvent OrdersNew for first order
+    // 检查第二个订单的AccountEvent OrdersNew
     match event_account_rx.try_recv() {
         Ok(AccountEvent {
-            kind: AccountEventKind::OrdersNew(new_orders),
-            ..
-        }) => {
+               kind: AccountEventKind::OrdersNew(new_orders),
+               ..
+           }) => {
             assert_eq!(new_orders.len(), 1);
             assert_eq!(new_orders[0].clone(), expected_order_new_2);
         }
@@ -1103,7 +1135,7 @@ async fn test_13_fail_to_open_one_of_two_limits_with_insufficient_funds(
         }
     }
 
-    // Check no more AccountEvents generated
+    // 检查是否没有更多的AccountEvents生成
     match event_account_rx.try_recv() {
         Err(mpsc::error::TryRecvError::Empty) => {}
         other => {
@@ -1115,9 +1147,12 @@ async fn test_13_fail_to_open_one_of_two_limits_with_insufficient_funds(
     }
 }
 
-// 14. Fail to cancel limit order with OrderNotFound using incorrect OrderId
+// 14. 使用错误的OrderId尝试取消限价订单并失败，因为找不到订单
+// 14. Fail to cancel limit order with OrderNotFound using incorrect OrderId.
 async fn test_14_fail_to_cancel_limit_with_order_not_found(client: &SimulatedExecution) {
+    // 生成一个新的客户端订单ID
     let cid = ClientOrderId(Uuid::new_v4());
+    // 尝试取消一个不存在的订单
     let cancelled = client
         .cancel_orders(vec![order_cancel_request(
             Instrument::from(("btc", "usdt", InstrumentKind::Perpetual)),
@@ -1127,8 +1162,11 @@ async fn test_14_fail_to_cancel_limit_with_order_not_found(client: &SimulatedExe
         )])
         .await;
 
+    // 预期结果为找不到订单的错误
     let expected = Err(ExecutionError::OrderNotFound(cid));
 
+    // 验证取消操作的结果
     assert_eq!(cancelled.len(), 1);
     assert_eq!(cancelled[0], expected);
 }
+
