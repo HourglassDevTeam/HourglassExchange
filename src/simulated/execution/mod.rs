9edee8bdcf8bf4188f3f35be94a6ed402fe707bc
arthur <arthur@ciuyat.com>
2024-07-02 00:12:36 +0800

    ini

--- src/simulated/execution/mod.rs
+++ src/simulated/execution/mod.rs
@@ -26,53 +26,48 @@ impl ExecutionClient for SimulatedExecution {
     async fn init(request_tx: Self::Config, _: mpsc::UnboundedSender<AccountEvent>) -> Self {
         Self { request_tx }
     }
-
+    // 从模拟交易所接收开放订单的响应。
     async fn fetch_orders_open(&self) -> Result<Vec<Order<Open>>, ExecutionError> {
         let (response_tx, response_rx) = oneshot::channel();
         self.request_tx
             .send(SimulatedEvent::FetchOrdersOpen(response_tx))
             .expect("[UniLinkExecution] : 模拟交易所目前离线 - 发送获取开放订单FetchOrdersOpen请求失败");
-        // 从模拟交易所接收开放订单的响应。
         response_rx
             .await
             .expect("[UniLinkExecution] : 模拟交易所目前离线 - 接收获取开放订单 FetchOrdersOpen 响应失败")
     }
-
+    // 从模拟交易所接收账户余额的响应。
     async fn fetch_balances(&self) -> Result<Vec<SymbolBalance>, ExecutionError> {
         let (response_tx, response_rx) = oneshot::channel();
         self.request_tx
             .send(SimulatedEvent::FetchBalances(response_tx))
             .expect("[UniLinkExecution] : 模拟交易所目前离线 - 发送获取账户余额 FetchBalances 请求失败");
-        // 从模拟交易所接收账户余额的响应。
         response_rx
             .await
             .expect("[UniLinkExecution] : 模拟交易所目前离线 - 接收获取账户余额 FetchBalances 响应失败")
     }
-
+    // 从模拟交易所接收开启订单的响应。
     async fn open_orders(&self, open_requests: Vec<Order<RequestOpen>>) -> Vec<Result<Order<Open>, ExecutionError>> {
         let (response_tx, response_rx) = oneshot::channel();
         self.request_tx
             .send(SimulatedEvent::OpenOrders((open_requests, response_tx)))
             .expect("[UniLinkExecution] : 模拟交易所目前离线 - 发送 OpenOrders 请求失败");
-        // 从模拟交易所接收开启订单的响应。
         response_rx.await.expect("[UniLinkExecution] : 模拟交易所目前离线 - 接收 OpenOrders 响应失败")
     }
-
+    // 从模拟交易所接收取消订单的响应。
     async fn cancel_orders(&self, cancel_requests: Vec<Order<RequestCancel>>) -> Vec<Result<Order<Cancelled>, ExecutionError>> {
         let (response_tx, response_rx) = oneshot::channel();
         self.request_tx
             .send(SimulatedEvent::CancelOrders((cancel_requests, response_tx)))
             .expect("[UniLinkExecution] : 模拟交易所目前离线 - 发送 CancelOrders 请求失败");
-        // 从模拟交易所接收取消订单的响应。
         response_rx.await.expect("[UniLinkExecution] : 模拟交易所目前离线 - 接收 CancelOrders 响应失败")
     }
-
+    // 从模拟交易所接收取消所有订单的响应。
     async fn cancel_orders_all(&self) -> Result<Vec<Order<Cancelled>>, ExecutionError> {
         let (response_tx, response_rx) = oneshot::channel();
         self.request_tx
             .send(SimulatedEvent::CancelOrdersAll(response_tx))
             .expect("[UniLinkExecution] : 模拟交易所目前离线 - 发送 CancelOrdersAll 请求失败");
-        // 从模拟交易所接收取消所有订单的响应。
         response_rx
             .await
             .expect("[UniLinkExecution] : 模拟交易所目前离线 - 接收 CancelOrdersAll 响应失败")
