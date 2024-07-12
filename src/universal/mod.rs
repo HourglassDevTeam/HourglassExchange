9b1311d91da34d5e21a5a29e3dfe48ddd06cddf1
arthur <arthur@ciuyat.com>
2024-07-12 11:56:02 +0800

    change

--- src/universal/mod.rs
+++ src/universal/mod.rs
@@ -1,4 +1,4 @@
-pub mod balance; // 定义balance模块
+pub mod balance; // 通用balance模块
 pub mod event_and_status; // 定义通用事件和状态
-pub mod order; // 定义订单模块
-pub mod trade; // 定义交易模块
+pub mod order; // 通用订单模块
+pub mod trade; // 通用交易模块
