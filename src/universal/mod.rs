a312baab24a6ec1ae56b2738156ee583ac867db1
arthur <arthur@ciuyat.com>
2024-07-12 12:06:05 +0800

    change

--- src/universal/mod.rs
+++ src/universal/mod.rs
@@ -1,4 +1,5 @@
 pub mod balance; // 通用balance模块
 pub mod event; // 定义通用事件和状态
 pub mod order; // 通用订单模块
-pub mod trade; // 通用交易模块
+pub mod trade;
+mod status; // 通用交易模块
---
+++ src/universal/status.rs
@@ -0,0 +1,15 @@
+
+#[allow(dead_code)]
+#[derive(Clone, Copy, Debug)]
+pub enum ClientStatus {
+    Connected,  // 已连接
+    CancelOnly, // 仅取消
+    Disconnected,
+    // Pending,      // 待定，正在尝试连接
+    // Suspended,    // 已暂停，暂时禁止所有操作
+    // Reconnecting, // 正在重连
+    // Error,        // 发生错误，无法正常操作
+    // Maintenance,  // 维护模式，系统暂时不可用
+    // Authenticated, // 已认证，已通过身份验证
+    // Unauthorized, // 未授权，身份验证失败或权限不足
+}
