9da96b3a9392e305e850f8524b275d146b7e5ac2
arthur <arthur@ciuyat.com>
2024-07-12 12:51:36 +0800

    `change`

--- src/universal/order.rs
+++ src/universal/order.rs
@@ -128,7 +128,7 @@ pub struct Pending;
 /// 在RequestCancel结构体中只记录OrderId的原因主要是因为取消订单操作通常只需要知道哪个订单需要被取消。
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
 pub struct RequestCancel {
-    pub id: OrderId,
+    pub id: OrderId, // Consider : 需要记录 CID 吗 ????
 }
 
 impl<Id> From<Id> for RequestCancel
