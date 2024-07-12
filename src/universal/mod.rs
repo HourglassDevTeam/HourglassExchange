416daa12c0f2027eeed866f387d8304f84634aac
arthur <arthur@ciuyat.com>
2024-07-12 17:42:37 +0800

    universal balance and position modules

--- src/universal/mod.rs
+++ src/universal/mod.rs
@@ -1,3 +1,6 @@
+use std::fmt::{Display, Formatter};
+use serde::{Deserialize, Serialize};
+
 pub mod balance; // 通用balance模块
 pub mod event; // 定义通用事件和状态
 pub mod friction;
@@ -5,4 +8,22 @@ pub mod order; // 通用订单模块
 pub mod position;
 pub mod status;
 pub mod trade;
+
+
 // 通用交易模块
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
+pub enum Side {
+    #[serde(alias = "buy", alias = "BUY", alias = "b")]
+    Buy,
+    #[serde(alias = "sell", alias = "SELL", alias = "s")]
+    Sell,
+}
+
+impl Display for Side {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            | Side::Buy => "buy",
+            | Side::Sell => "sell",
+        })
+    }
+}
