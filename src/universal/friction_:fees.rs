adec65d7bd8f294fb0f170c96ecac5e037053183
arthur <arthur@ciuyat.com>
2024-07-12 12:33:56 +0800

    `change`

---
+++ src/universal/friction_:fees.rs
@@ -0,0 +1,17 @@
+#[allow(dead_code)]
+/// 以 [`Symbol`]（符号）表示的 [`Trade`]（交易）费用。
+#[derive(Clone, PartialEq, PartialOrd, Debug, Deserialize, Serialize)]
+pub struct SymbolFees {
+    pub symbol: Symbol,
+    pub fees: f64,
+}
+
+impl SymbolFees {
+    /// 构造一个新的 [`SymbolFees`]。
+    pub fn new<S>(symbol: S, fees: f64) -> Self
+                  where
+                      S: Into<Symbol>,
+    {
+        Self { symbol: symbol.into(), fees }
+    }
+}
