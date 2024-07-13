427c108f81337886169e77e97fbf16ced561110e
arthur <arthur@ciuyat.com>
2024-07-13 15:33:20 +0800

    changes

---
+++ src/universal/data/mod.rs
@@ -0,0 +1,19 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+pub mod historical;
+pub mod live;
+
+
+pub trait FeedGenerator<Event>
+{
+    fn next(&mut self) -> Feed<Event>;
+}
+
+#[derive(Clone, Eq, PartialEq, PartialOrd, Debug, Deserialize, Serialize)]
+pub enum Feed<Event>
+{
+    Next(Event),
+    Unhealthy,
+    Finished,
+}
+
