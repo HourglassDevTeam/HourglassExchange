f9661de39867c8dcf811282266e61c10adf4a11a
arthur@ciuyat.com <arthur@ciuyat.com>
2024-07-14 00:13:53 +0800

    update

--- src/simulated/account_config.rs
+++ src/simulated/account_config.rs
@@ -28,3 +28,43 @@ pub enum CommissionLevel {
     Lv5,
     // ..........
 }
+
+
+pub struct AccountConfigBuilder {
+    margin_mode: Option<MarginMode>,
+    position_mode: Option<PositionMode>,
+    commission_level: Option<CommissionLevel>,
+}
+
+impl AccountConfigBuilder {
+    pub fn new() -> Self {
+        Self {
+            margin_mode: None,
+            position_mode: None,
+            commission_level: None,
+        }
+    }
+
+    pub fn margin_mode(mut self, margin_mode: MarginMode) -> Self {
+        self.margin_mode = Some(margin_mode);
+        self
+    }
+
+    pub fn position_mode(mut self, position_mode: PositionMode) -> Self {
+        self.position_mode = Some(position_mode);
+        self
+    }
+
+    pub fn commission_level(mut self, commission_level: CommissionLevel) -> Self {
+        self.commission_level = Some(commission_level);
+        self
+    }
+
+    pub fn build(self) -> Result<AccountConfig, &'static str> {
+        Ok(AccountConfig {
+            margin_mode: self.margin_mode.ok_or("margin_mode is required")?,
+            position_mode: self.position_mode.ok_or("position_mode is required")?,
+            commission_level: self.commission_level.ok_or("commission_level is required")?,
+        })
+    }
+}
