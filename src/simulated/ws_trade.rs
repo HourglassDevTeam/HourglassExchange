d24dc4922e766a678d05dcaa63e316f9db8e4a1e
arthur@ciuyat.com <arthur@ciuyat.com>
2024-07-14 20:24:20 +0800

    update

--- src/simulated/ws_trade.rs
+++ src/simulated/ws_trade.rs
@@ -1,9 +1,14 @@
 // 回测专用的模拟WebSocket交易信息，只有4个字段
 use serde::{Deserialize, Serialize};
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct WsTrade {
+    #[serde(alias = "instrument_id", alias = "inst_id")]
     instId: String,
+    #[serde(alias = "side")]
     side: String,
+    #[serde(alias = "price")]
     px: String,
+    #[serde(alias = "timestamp")]
     ts: String,
 }
