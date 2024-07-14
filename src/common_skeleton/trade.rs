e839da9ba4bef567633415b354d006bcd3cbb118
arthur@ciuyat.com <arthur@ciuyat.com>
2024-07-14 20:13:47 +0800

    update

--- src/common_skeleton/trade.rs
+++ src/common_skeleton/trade.rs
@@ -3,7 +3,6 @@ use serde::{Deserialize, Serialize};
 // 引入相关模块和结构体。
 use crate::common_skeleton::{instrument::Instrument, Side};
 
-/// 标准化 [`Trade`]。
 #[derive(Clone, PartialEq, PartialOrd, Debug, Deserialize, Serialize)]
 pub struct Trade
 {
--- src/simulated/ws_trade_converter.rs
+++ src/simulated/ws_trade_converter.rs
@@ -1,1 +1,9 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct WsTrade {
+    instId: String,
+    side: String,
+    px: String,
+    ts: String,
+}
