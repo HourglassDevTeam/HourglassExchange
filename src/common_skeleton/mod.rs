67892751df999ee9831e094fa27f5025bd626930
arthur@ciuyat.com <arthur@ciuyat.com>
2024-07-14 14:27:08 +0800

    update

--- src/common_skeleton/mod.rs
+++ src/common_skeleton/mod.rs
@@ -7,13 +7,12 @@ pub mod data;
 pub mod event; // 定义通用事件和状态
 pub mod friction;
 pub mod instrument;
-pub mod order; // 通用订单模块
+pub mod order;
 pub mod position;
 pub mod status;
 pub mod token;
 pub mod trade;
 
-// 通用交易模块
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
 pub enum Side {
     #[serde(alias = "buy", alias = "BUY", alias = "b")]
