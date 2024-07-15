0b4f12d8d581e184ac310771267d912b5676d163
arthur@ciuyat.com <arthur@ciuyat.com>
2024-07-15 20:59:24 +0800

    update

--- src/simulated_exchange/account/account_datafeed.rs
+++ src/simulated_exchange/account/account_datafeed.rs
@@ -1,9 +1,8 @@
 use crate::common_skeleton::datafeed::historical::MarketFeed;
-use serde::Serialize;
 use uuid::Uuid;
 
 // 鉴于Data的种类可能会很多，规避避开enum的开销和维护成本，使用泛型来定义AccountFeedData类型。
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub struct AccountMarketFeed<Iter, Event>
     where Iter: Iterator<Item = Event>+Clone
 {
