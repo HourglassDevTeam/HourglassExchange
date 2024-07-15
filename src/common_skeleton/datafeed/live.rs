f9372b0c04ba032991744b1196bb1a141bf5f8b1
arthur@ciuyat.com <arthur@ciuyat.com>
2024-07-15 21:30:06 +0800

    update

--- src/common_skeleton/datafeed/live.rs
+++ src/common_skeleton/datafeed/live.rs
@@ -1,4 +1,5 @@
 use tokio::sync::mpsc;
+use crate::common_skeleton::datafeed::Feed;
 
 #[derive(Debug)]
 pub struct LiveFeed<Event>
@@ -6,3 +7,19 @@ pub struct LiveFeed<Event>
     pub market_rx: mpsc::UnboundedReceiver<Event>,
 }
 
+
+impl<Event> LiveFeedGenerator<Event> for LiveFeed<Event>
+{
+    /// 实现 MarketGenerator trait，用于生成下一个市场 `Event`。
+
+    fn next(&mut self) -> Feed<Event>
+    {
+        loop {
+            match self.market_rx.try_recv() {
+                | Ok(event) => break Feed::Next(event),
+                | Err(mpsc::error::TryRecvError::Empty) => continue,
+                | Err(mpsc::error::TryRecvError::Disconnected) => break Feed::Finished,
+            }
+        }
+    }
+}
