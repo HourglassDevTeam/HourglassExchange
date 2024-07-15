561ea44ba29dfadcbae9130451f1b3b83faa27d9
arthur@ciuyat.com <arthur@ciuyat.com>
2024-07-15 22:31:24 +0800

    update

--- src/simulated_exchange/client.rs
+++ src/simulated_exchange/client.rs
@@ -15,7 +15,7 @@ pub struct SimulatedClient {
 #[async_trait]
 impl ClientExecution for SimulatedClient {
     // very naturally, the client's kind is determined by the exchange.
-    const CLIENT_KIND = ExchangeKind::Simulated;
+    const CLIENT_KIND :ExchangeKind = ExchangeKind::Simulated;
     // in our case the 'optional' config parameter in the simulated exchange is an UnboundedSender
     type Config = UnboundedSender<SimulatedEvent>;
 
