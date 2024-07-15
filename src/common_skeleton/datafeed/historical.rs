abfec800f8f55f2951834c459323b91c7bad37ad
arthur@ciuyat.com <arthur@ciuyat.com>
2024-07-15 21:38:54 +0800

    update

--- src/common_skeleton/datafeed/historical.rs
+++ src/common_skeleton/datafeed/historical.rs
@@ -1,19 +1,28 @@
 use crate::common_skeleton::datafeed::{Feed, MarketFeedDistributor};
+// 从 common_skeleton 模块中引入 Feed 和 MarketFeedDistributor
+// 这两个结构体或特性用于处理市场数据
 
 /// 历史市场事件的 [`Feed`]。
 /// MarketFeed 接受一个泛型迭代器[Iter]，并允许用户按需逐个获取历史市场事件。
 /// 这种方式适合处理离线数据或在内存中加载整个历史数据集的情况。
 #[derive(Debug)]
 pub struct HistoricalFeed<Iter, Event>
-    where Iter: Iterator<Item = Event>
+where Iter: Iterator<Item = Event>
 {
-    pub market_iterator: Iter,
+    // 定义一个结构体 HistoricalFeed，带有两个泛型参数 Iter 和 Event
+    // Iter 必须是一个迭代器，且其迭代项是 Event 类型的元素
+    pub market_iterator: Iter, // 这个字段是一个迭代器，用于按需获取历史市场事件
 }
 
-impl<Iter, Event> MarketFeedDistributor<Event> for HistoricalFeed<Iter, Event> where Iter: Iterator<Item = Event>
+impl<Iter, Event> MarketFeedDistributor<Event> for HistoricalFeed<Iter, Event>
+where Iter: Iterator<Item = Event>
 {
+    // 为 HistoricalFeed 实现 MarketFeedDistributor 特性
     fn fetch_next(&mut self) -> Feed<Event>
     {
+        // fetch_next 方法尝试从迭代器中获取下一个事件
+        // 如果有事件，返回 Feed::Next(event)
+        // 如果没有事件，返回 Feed::Finished
         self.market_iterator.next().map_or(Feed::Finished, Feed::Next)
     }
 }
@@ -21,9 +30,12 @@ impl<Iter, Event> MarketFeedDistributor<Event> for HistoricalFeed<Iter, Event> w
 // HistoricalFeed生成的办法，新建一个接受Event泛型的历史市场事件迭代器
 impl<Iter, Event> HistoricalFeed<Iter, Event> where Iter: Iterator<Item = Event>
 {
+    // 为 HistoricalFeed 实现一个新的关联函数 initiate
+    // 这个函数用于从一个实现了 IntoIterator 特性的类型创建 HistoricalFeed
     pub fn initiate<IntoIter>(market_iterator: IntoIter) -> Self
-        where IntoIter: IntoIterator<Item = Event, IntoIter = Iter>
+    where IntoIter: IntoIterator<Item = Event, IntoIter = Iter>
     {
+        // 使用提供的迭代器创建一个新的 HistoricalFeed 实例
         Self { market_iterator: market_iterator.into_iter() }
     }
 }
