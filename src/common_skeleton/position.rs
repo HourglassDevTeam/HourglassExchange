1375e858b2ed472fbafd50e871d3cb5ad8976746
arthur <arthur@ciuyat.com>
2024-07-15 17:31:34 +0800

    注释骨架中的position模块。日后需要和交易所API严密核对。

--- src/common_skeleton/position.rs
+++ src/common_skeleton/position.rs
@@ -1,6 +1,8 @@
 // FIXME : code below needs to be restructured and fitted to the framework. need to provide enums?
 // CONSIDER: can these positions coexist, if so enums might not be ideal.
 
+use chrono::{DateTime, Utc};
+
 #[allow(dead_code)]
 #[derive(Clone, Debug)]
 pub struct AccountPositions
@@ -12,17 +14,6 @@ pub struct AccountPositions
 }
 
 
-
-// NOTE 如果确实需要多种头寸类型共存，可以考虑如下设计：
-#[allow(dead_code)]
-#[derive(Clone, Debug)]
-pub enum Position {
-    Swap(SwapPosition),
-    // 如果未来需要添加其他类型，可以取消注释以下行：
-    // Margin(MarginPosition),
-    // Futures(FuturesPosition),
-    // Option(OptionPosition),
-}
 
 
 #[derive(Clone, Debug)]
@@ -56,3 +47,59 @@ pub enum PositionMarginMode
     Cross,
     Isolated,
 }
+
+
+// NOTE 如果确实需要多种头寸类型共存，可以考虑如下设计：
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+pub enum Position {
+    Swap(SwapPosition),
+    // 如果未来需要添加其他类型，可以取消注释以下行：
+    // Margin(MarginPosition),
+    // Futures(FuturesPosition),
+    // Option(OptionPosition),
+}
+
+
+// NOTE 此处为一个尽量详细的元Position案例
+// #[derive(Debug, Default, Clone)]
+// pub struct MetaPosition{
+//     // 交易所时间，使用DateTime<Utc>类型，表示与交易所同步的UTC时间
+//     pub exchange_time: DateTime<Utc>,
+//     // 头寸ID，Option类型，表示可能不存在头寸ID的情况
+//     pub position_id: Option<PositionID>,
+//     // 交易所信息，Option类型，表示头寸可能没有具体交易所的标记
+//     pub exchange: Option<Exchange>,
+//     // 交易工具，Option类型，表示头寸相关的交易工具或资产
+//     pub instrument: Option<Instrument>,
+//     // 头寸元数据，Option类型，包含头寸的额外信息
+//     pub meta: Option<PositionMeta>,
+//     // 交易方向，Option类型，可以是多头(buy)或空头(sell)
+//     pub side: Option<Side>,
+//     // 头寸数量，Option类型，表示持有的资产数量
+//     pub quantity: Option<f64>,
+//     // 入场交易费用，Option类型，表示交易时产生的费用
+//     pub enter_fees: Option<CryptoFriction>,
+//     // 入场总费用，Option类型，表示入场交易的总费用
+//     pub enter_fees_total: Option<Fees>,
+//     // 入场平均价格（毛），Option类型，表示考虑交易费用后的平均入场价格
+//     pub enter_avg_price_gross: Option<f64>,
+//     // 入场价值（毛），Option类型，表示头寸入场时的总价值
+//     pub enter_value_gross: Option<f64>,
+//     // 退出交易费用，Option类型，表示退出头寸时产生的费用
+//     pub exit_fees: Option<CryptoFriction>,
+//     // 退出总费用，Option类型，表示退出交易的总费用
+//     pub exit_fees_total: Option<Fees>,
+//     // 退出平均价格（毛），Option类型，表示考虑交易费用后的退出平均价格
+//     pub exit_avg_price_gross: Option<f64>,
+//     // 退出价值（毛），Option类型，表示退出头寸时的总价值
+//     pub exit_value_gross: Option<f64>,
+//     // 当前市场价格，Option类型，表示当前头寸资产的市场价格
+//     pub current_symbol_price: Option<f64>,
+//     // 当前价值（毛），Option类型，表示头寸当前的总价值
+//     pub current_value_gross: Option<f64>,
+//     // 未实现盈亏，Option类型，表示头寸当前的未实现利润或亏损
+//     pub unrealised_profit_loss: Option<f64>,
+//     // 已实现盈亏，Option类型，表示头寸已退出部分的利润或亏损
+//     pub realised_profit_loss: Option<f64>,
+// }
