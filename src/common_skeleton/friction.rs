d1dd2bf4c03fa5bca2c1e683b1bdeea4507dad33
arthur <arthur@ciuyat.com>
2024-07-15 17:39:54 +0800

    将TokenFees改为InstrumentFees。想根据不同的交易对（包括基础货币、报价货币，可能还有交易类型）来定义交易费用，那么使用 InstrumentFees 可能是一个更合适的选择。

--- src/common_skeleton/friction.rs
+++ src/common_skeleton/friction.rs
@@ -1,24 +1,23 @@
 // NOTE 滑点和摩擦成本的设计放在这里
 
 use serde::{Deserialize, Serialize};
-
-use crate::common_skeleton::token::Token;
+use crate::common_skeleton::instrument::kind::InstrumentKind;
 
 #[allow(dead_code)]
-/// 以 [`Token`]（符号）表示的 [`Trade`]（交易）费用。
+/// 以 [`Instrument`]（符号）表示的 [`Trade`]（交易）费用。
 #[derive(Clone, PartialEq, PartialOrd, Debug, Deserialize, Serialize)]
-pub struct TokenFees
+pub struct InstrumentFees
 {
-    pub token: Token,
+    pub instrument_kind: InstrumentKind,
     pub fees: f64,
 }
 
-impl TokenFees
+impl InstrumentFees
 {
-    /// 构造一个新的 [`TokenFees`]。
-    pub fn new<S>(token: S, fees: f64) -> Self
-        where S: Into<Token>
+    /// 构造一个新的 [`InstrumentFees`]。
+    pub fn new<S>(instrument: S, fees: f64) -> Self
+        where S: Into<InstrumentKind>
     {
-        Self { token: token.into(), fees }
+        Self { instrument_kind: instrument.into(), fees }
     }
 }
