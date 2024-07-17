ad332b8287855861ea7d446ed3eb524062b8f7fd
arthur@ciuyat.com <arthur@ciuyat.com>
2024-07-17 21:59:42 +0800

    formatted

--- examples/historical_stream_designed.rs
+++ examples/historical_stream_designed.rs
@@ -2,11 +2,9 @@ use std::sync::Arc;
 use lazy_static::lazy_static;
 use unilink_execution::{common_skeleton::datafeed::historical::HistoricalFeed, simulated_exchange::load_from_clickhouse::queries_operations::*};
 
-
 lazy_static! {
     pub static ref CLIENT: Arc<ClickHouseClient> = Arc::new(ClickHouseClient::new());
 }
-
 
 #[tokio::main]
 async fn main()
@@ -15,7 +13,6 @@ async fn main()
     let instrument = "futures";
     let channel = "trades";
     let date = "2024_03_03";
-
     let stream = CLIENT.query_union_table_batched(exchange, instrument, channel, date);
     HistoricalFeed { database_client: CLIENT.to_owned(), stream: Box::pin(stream) };
 }
