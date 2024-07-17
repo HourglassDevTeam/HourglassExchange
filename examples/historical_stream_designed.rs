d1914144e747e8d2fb79aca649f5ca4be9e0ca19
arthur <arthur@ciuyat.com>
2024-07-17 18:31:16 +0800

    add enum StreamKind for AccountMarketFeed to choose

--- examples/historical_stream_designed.rs
+++ examples/historical_stream_designed.rs
@@ -1,5 +1,6 @@
 use unilink_execution::common_skeleton::datafeed::historical::HistoricalFeed;
 use unilink_execution::simulated_exchange::load_from_clickhouse::queries_operations::ClickHouseClient;
+use unilink_execution::simulated_exchange::ws_trade::WsTrade;
 
 // Usage of `query_union_table_batched` to create a `HistoricalFeed`
 #[tokio::main]
@@ -11,6 +12,6 @@ async fn main() {
     let date = "2024_03_03";
 
     let stream = client.query_union_table_batched(exchange, instrument, channel, date);
-    HistoricalFeed { stream: Box::pin(stream) };
+    let feed: HistoricalFeed<WsTrade> = HistoricalFeed::new(client, "binance", "futures", "trades", "2024_03_03");
 
 }
--- src/common_skeleton/datafeed/historical.rs
+++ src/common_skeleton/datafeed/historical.rs
@@ -1,19 +1,35 @@
 use std::pin::Pin;
+use std::sync::Arc;
 
-use futures::Stream;
-
+use futures::{stream, Stream, StreamExt};
 use crate::error::ExecutionError;
 use crate::simulated_exchange::load_from_clickhouse::queries_operations::ClickHouseClient;
 
 pub struct HistoricalFeed<Event>
 {
     // NOTE ClickHouseClient is opted and hardcoded for simplicity, as a makeshift solution.
-    pub database_client: ClickHouseClient,
+    pub database_client: Arc<ClickHouseClient>,
     pub stream: Pin<Box<dyn Stream<Item = Result<Event, ExecutionError>> + Send>>,
 }
 
 impl<Event> HistoricalFeed<Event> where Event: Clone + Send + Sync + 'static
 {
+
+    pub fn new(client: Arc<ClickHouseClient>, exchange: &str, instrument: &str, channel: &str, date: &str) -> Self {
+        let initial_stream = client.query_union_table_batched(exchange, instrument, channel, date);
+        // Transform Vec<WsTrade> into a stream of WsTrade
+        let flattened_stream = initial_stream.flat_map(|result_vec| {
+            match result_vec {
+                Ok(vec) => stream::iter(vec.into_iter().map(Ok)).boxed(),
+                Err(e) => stream::once(async move { Err(e) }).boxed(),
+            }
+        });
+
+        HistoricalFeed {
+            database_client: client,
+            stream: Box::pin(flattened_stream),
+        }
+    }
     pub fn poll_next(&mut self) -> Pin<&mut (dyn Stream<Item = Result<Event, ExecutionError>> + Send)>
     {
         self.stream.as_mut()
