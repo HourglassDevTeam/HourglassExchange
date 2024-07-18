1228bea02eebf8f2752217ccfa0b2de0983b8ca5
arthur <arthur@ciuyat.com>
2024-07-18 18:05:33 +0800

    add enum StreamKind for AccountMarketFeed to choose

--- src/common_skeleton/instrument/kind.rs
+++ src/common_skeleton/instrument/kind.rs
@@ -1,7 +1,4 @@
 use std::fmt::{Display, Formatter};
-
-use chrono::{DateTime, Utc};
-use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
@@ -11,8 +8,8 @@ pub enum InstrumentKind
     Spot, // [NOTE] 注意：Spot 指的是即期合约，此处现在缺乏合约细节字段，不适合MarketID的唯一识别。
     #[serde(alias = "Swap", alias = "SWAP", alias = "PERPETUAL")]
     Perpetual, // [NOTE] 注意：Perpetual 指的是永续合约，此处现缺乏合约细节字段，不适合MarketID的唯一识别。
-    Future(FutureContract),
-    Option(OptionContract),
+    Future,
+    Option,
 }
 
 impl Default for InstrumentKind
@@ -29,80 +26,13 @@ impl Display for InstrumentKind
     {
         match self {
             | InstrumentKind::Spot => write!(f, "spot"),
-            | InstrumentKind::Future(future) => {
-                write!(f, "{}_{}", future.future_code, future.multiplier,)
+            | InstrumentKind::Future => {
+                write!(f, "future")
             }
             | InstrumentKind::Perpetual => write!(f, "perpetual"),
-            | InstrumentKind::Option(option) => {
-                write!(f, "{}_{}", option.option_code, option.multiplier,)
+            | InstrumentKind::Option => {
+                write!(f, "option")
             }
         }
-    }
-}
-
-/// [InstrumentKind::Option] 合约的配置。
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Deserialize, Serialize)]
-pub struct OptionContract
-{
-    pub option_code: String,
-    pub direction: OptionSide,    // call或者put
-    pub exercise: OptionExercise, // 美式或者欧式
-    #[serde(with = "chrono::serde::ts_milliseconds")]
-    pub maturity: DateTime<Utc>, // 到期日
-    pub strike: Decimal,          // 行权价格
-    #[serde(with = "chrono::serde::ts_milliseconds")]
-    pub listeddate: DateTime<Utc>, // 上市日期
-    pub multiplier: u32,
-}
-
-/// [InstrumentKind::Future] 合约的配置。
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Deserialize, Serialize)]
-pub struct FutureContract
-{
-    pub future_code: String,
-    // pub maturity: DateTime<Utc>, //到期日, not necessary currently
-    pub multiplier: u32,
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Deserialize, Serialize)]
-#[serde(rename_all = "lowercase")]
-pub enum OptionSide
-{
-    #[serde(alias = "CALL", alias = "Call", alias = "C")]
-    Call,
-    #[serde(alias = "PUT", alias = "Put", alias = "P")]
-    Put,
-}
-
-impl Display for OptionSide
-{
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result
-    {
-        write!(f, "{}", match self {
-            | OptionSide::Call => "call",
-            | OptionSide::Put => "put",
-        })
-    }
-}
-
-/// [OptionContract] 行权方式。
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Deserialize, Serialize)]
-#[serde(rename_all = "lowercase")]
-pub enum OptionExercise
-{
-    #[serde(alias = "AMERICAN", alias = "American", alias = "美式", alias = "美式期权")]
-    American,
-    #[serde(alias = "EUROPEAN", alias = "European", alias = "欧式", alias = "欧式期权")]
-    European,
-}
-
-impl Display for OptionExercise
-{
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result
-    {
-        write!(f, "{}", match self {
-            | OptionExercise::American => "american",
-            | OptionExercise::European => "european",
-        })
     }
 }
