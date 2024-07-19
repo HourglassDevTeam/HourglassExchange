e470d2dc9461a538f8a67a3e628c420309a1ef86
arthur <arthur@ciuyat.com>
2024-07-19 16:51:14 +0800

    in progress

--- src/data_subscriber/mod.rs
+++ src/data_subscriber/mod.rs
@@ -21,19 +21,16 @@ mod websocket;
 #[derive(Debug)]
 pub struct SubscriptionId(pub String);
 
-/// `SubscriptionMeta` 结构体
-/// 还包括一个 `Vec`，用于存储 WebSocket 消息（`WsMessage`）。
 pub struct SubscriptionMeta
 {
-    /// `HashMap`，包含 [`SubscriptionId`] 和它关联的  [`Instrument`] 之间的映射。
     pub instrument_map: SubscriptionMap<Instrument>,
     /// 存储 WebSocket 消息的向量。
     pub subscriptions: Vec<WsMessage>,
 }
 
-/// `Map` 结构体
 /// 用于存储 [`SubscriptionId`] 与泛型类型 `T` 之间的映射。
 pub struct SubscriptionMap<T>(pub HashMap<SubscriptionId, T>);
+
 /// 使用 tokio-tungstenite 库的 [WebSocketStream]，可能是 TLS 或非 TLS 的 TcpStream。
 pub type WebSocket = tokio_tungstenite::WebSocketStream<MaybeTlsStream<TcpStream>>;
 
