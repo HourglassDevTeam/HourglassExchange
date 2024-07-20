26ba14167320f22a4b77529b0c80a0532801cccd
arthur <arthur@ciuyat.com>
2024-07-20 18:05:48 +0800

    adding live_stream test

--- src/common_skeleton/datafeed/live.rs
+++ src/common_skeleton/datafeed/live.rs
@@ -2,6 +2,7 @@ use std::fmt::Debug;
 
 use mpsc::UnboundedReceiver;
 use tokio::sync::mpsc;
+use tokio::sync::mpsc::UnboundedSender;
 
 use crate::common_skeleton::datafeed::event::MarketEvent;
 
@@ -18,5 +19,15 @@ impl<Event> LiveFeed<Event> where Event: Clone + Send + Sync + Debug + 'static
     {
         // 尝试从接收器中接收事件
         self.receiver.try_recv().ok()
+    }
+}
+
+
+impl<Event> LiveFeed<Event> {
+    /// Creates a new `LiveFeed`.
+    pub fn new(sender: UnboundedSender<MarketEvent<Event>>) -> Self {
+        LiveFeed {
+            receiver: mpsc::unbounded_channel().1,
+        }
     }
 }
