70734a1f6e40c5417f78ee44e073dbe99641f690
arthur <arthur@ciuyat.com>
2024-07-20 15:07:09 +0800

    in progress

--- src/common_skeleton/datafeed/live.rs
+++ src/common_skeleton/datafeed/live.rs
@@ -1,12 +1,8 @@
 use std::fmt::Debug;
 
-use futures::StreamExt;
 use mpsc::UnboundedReceiver;
 use tokio::sync::mpsc;
 
-use crate::{
-    simulated_exchange::account::account_market_feed::Subscription,
-};
 use crate::common_skeleton::datafeed::event::MarketEvent;
 
 /// Live feed for events.
@@ -21,19 +17,5 @@ impl<Event> LiveFeed<Event> where Event: Clone + Send + Sync + Debug + 'static
     {
         // 尝试从接收器中接收事件
         self.receiver.try_recv().ok()
-    }
-}
-
-impl<Event> LiveFeed<Event> where Event: Clone + Send + Sync + Debug + 'static + Ord
-{
-    pub async fn new<Exchange, SubscriptionKind>(subscriptions: &[Subscription<Exchange, SubscriptionKind>]) -> Result<Self, SocketError>
-        where Exchange: Connector + Send + Sync,
-              SubscriptionKind: SubKind + Send + Sync,
-              Subscription<Exchange, SubscriptionKind>: Identifier<Exchange::Channel> + Identifier<Exchange::Market>
-    {
-        let (websocket, _instrument_map) = WebSocketSubscriber::subscribe(subscriptions).await?;
-        let stream = websocket.map(|msg| Event::parse_ws(msg)).boxed();
-
-        Ok(Self { receiver: stream })
     }
 }
