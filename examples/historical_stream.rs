2bf047d86a85b7607c962aa38c6ac72da6bc1af5
arthur <arthur@ciuyat.com>
2024-07-20 17:25:13 +0800

    now unbounded channel is working again

--- examples/historical_stream.rs
+++ examples/historical_stream.rs
@@ -17,21 +17,21 @@ async fn main() {
     let client = Arc::new(ClickHouseClient::new());
 
     // 定义交易所、金融工具、频道和日期的字符串变量
-    let stream_params = vec![("binance", "futures", "trades", "2024_03_03", 1000000)];
+    let stream_params = vec![("binance", "futures", "trades", "2024_03_03", "2024_07_03",1000000)];
 
     // 创建 AccountMarketStreams 实例
     let mut account_streams: AccountDataStreams<MarketEvent<ClickhouseTrade>> = AccountDataStreams::new();
 
-    for (exchange, instrument, channel, date, batch_size) in stream_params {
+    for (exchange, instrument, channel, start_date,end_date, batch_size) in stream_params {
         let client = client.clone();
         match client
-            .query_unioned_trade_table_batched_for_dates(exchange, instrument, channel, date, date, batch_size)
+            .query_unioned_trade_table_batched_for_dates(exchange, instrument, channel, start_date, end_date, batch_size)
             .await
         {
             Ok(mut rx) => {
                 let (tx, rx_clone) = unbounded_channel::<MarketEvent<ClickhouseTrade>>();
 
-                let stream_id = format!("{}_{}_{}_{}", exchange, instrument, channel, date);
+                let stream_id = format!("{}_{}_{}", exchange, instrument, channel);
                 account_streams.add_stream(stream_id.clone(), rx_clone);
                 println!("Hooooray! New stream has been added.");
 
