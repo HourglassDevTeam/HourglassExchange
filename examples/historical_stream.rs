ddca98bd0115aefac464ecb05e5205506f4490a6
arthur <arthur@ciuyat.com>
2024-07-20 20:53:18 +0800

    test

--- examples/historical_stream.rs
+++ examples/historical_stream.rs
@@ -1,16 +1,16 @@
-use std::sync::Arc;
 use lazy_static::lazy_static;
-use tokio::sync::mpsc::unbounded_channel;
-use tokio::sync::Mutex;
+use std::sync::Arc;
+use tokio::{
+    sync::{mpsc::unbounded_channel},
+    task,
+};
 use unilink_execution::{
     common_skeleton::datafeed::event::MarketEvent,
     simulated_exchange::{account::account_market_feed::*, load_from_clickhouse::queries_operations::*},
 };
-use tokio::task;
 lazy_static! {
     pub static ref CLIENT: Arc<ClickHouseClient> = Arc::new(ClickHouseClient::new());
 }
-
 
 #[tokio::main]
 async fn main() {
@@ -21,9 +21,6 @@ async fn main() {
 
     // 创建 AccountMarketStreams 实例
     let mut account_streams: AccountDataStreams<MarketEvent<ClickhouseTrade>> = AccountDataStreams::new();
-
-    // 创建一个 Arc<Mutex<HashMap>> 来存储所有的发送者
-    let tx_map = Arc::new(Mutex::new(std::collections::HashMap::new()));
 
     // 存储所有的异步任务句柄
     let mut handles = Vec::new();
@@ -36,24 +33,16 @@ async fn main() {
         // 在循环外部创建 unbounded_channel
         let (tx, rx_clone) = unbounded_channel::<MarketEvent<ClickhouseTrade>>();
 
-        // 将发送者插入到 HashMap 中
-        tx_map.lock().await.insert(stream_id.clone(), tx);
         account_streams.add_stream(stream_id.clone(), rx_clone);
-
-        // 克隆 Arc 指针以便在异步任务中使用
-        let tx_map = Arc::clone(&tx_map);
 
         // 创建异步任务并将句柄存储到 handles 向量中
         let handle = task::spawn(async move {
             match client.query_unioned_trade_table_batched_for_dates(exchange, instrument, channel, start_date, end_date, batch_size).await {
                 Ok(mut rx) => {
                     while let Some(event) = rx.recv().await {
-                        let tx_map = tx_map.lock();
-                        if let Some(tx) = tx_map.await.get(&stream_id) {
-                            if tx.send(event).is_err() {
-                                eprintln!("Failed to send event");
-                                break;
-                            }
+                        if tx.send(event).is_err() {
+                            eprintln!("Failed to send event");
+                            break;
                         }
                     }
                     println!("Finished processing events for stream: {}", stream_id);
