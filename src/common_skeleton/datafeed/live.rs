0a87480e52cb0eca7578b1a5ab2ca2df507d2a50
arthur <arthur@ciuyat.com>
2024-07-20 18:26:49 +0800

    adding live_stream test

--- src/common_skeleton/datafeed/live.rs
+++ src/common_skeleton/datafeed/live.rs
@@ -1,30 +1,30 @@
-use std::fmt::Debug;
-
-use mpsc::UnboundedReceiver;
-use tokio::sync::mpsc;
-use cerebro_data::event::MarketEvent;
-
-/// Live feed for events.
-#[allow(dead_code)]
-pub struct LiveFeed<Event>
-{
-    pub(crate) receiver: UnboundedReceiver<MarketEvent<Event>>,
-}
-
-impl<Event> LiveFeed<Event> where Event: Clone + Send + Sync + Debug + 'static
-{
-    pub async fn recv(&mut self) -> Option<MarketEvent<Event>>
-    {
-        // 尝试从接收器中接收事件
-        self.receiver.recv().await
-    }
-}
-
-
-impl<Event> LiveFeed<Event> {
-    pub fn new(receiver: UnboundedReceiver<MarketEvent<Event>>) -> Self {
-        LiveFeed {
-            receiver,
-        }
-    }
-}
+// use std::fmt::Debug;
+//
+// use mpsc::UnboundedReceiver;
+// use tokio::sync::mpsc;
+// use cerebro_data::event::MarketEvent;
+//
+// /// Live feed for events.
+// #[allow(dead_code)]
+// pub struct LiveFeed<Event>
+// {
+//     pub(crate) receiver: UnboundedReceiver<MarketEvent<Event>>,
+// }
+//
+// impl<Event> LiveFeed<Event> where Event: Clone + Send + Sync + Debug + 'static
+// {
+//     pub async fn recv(&mut self) -> Option<MarketEvent<Event>>
+//     {
+//         // 尝试从接收器中接收事件
+//         self.receiver.recv().await
+//     }
+// }
+//
+//
+// impl<Event> LiveFeed<Event> {
+//     pub fn new(receiver: UnboundedReceiver<MarketEvent<Event>>) -> Self {
+//         LiveFeed {
+//             receiver,
+//         }
+//     }
+// }
