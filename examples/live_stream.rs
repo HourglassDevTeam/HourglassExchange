3494e707f1e935ff525664740466ee8c66248635
arthur <arthur@ciuyat.com>
2024-07-20 18:02:19 +0800

    adding live_stream test

--- examples/live_stream.rs
+++ examples/live_stream.rs
@@ -1,31 +1,28 @@
 // 引入所需的模块
 use cerebro_data::{
-    event::{DataKind, MarketEvent},
     exchange::okx::Okx,
     streams::Streams,
     subscription::trade::PublicTrades,
 };
+use cerebro_data::exchange::ExchangeId;
 use cerebro_integration::model::instrument::kind::InstrumentKind;
-use tokio_stream::StreamExt;
 
-#[rustfmt::skip]
 #[tokio::main]
 async fn main() {
-    let streams: Streams<MarketEvent<DataKind>> = Streams::builder_multi()
+    let mut streams = Streams::<PublicTrades>::builder()
         // 添加 PublicTrades 流
-        .add(Streams::<PublicTrades>::builder()
-            .subscribe([
-                (Okx, "btc", "usdt", InstrumentKind::Spot, PublicTrades),
-                (Okx, "btc", "usdt", InstrumentKind::Perpetual, PublicTrades),
-            ])
-        )
+        .subscribe([
+        (Okx::default(), "btc", "usdt", InstrumentKind::Perpetual, PublicTrades),
+    ])
         .init()
         .await
         .unwrap();
 
-    let mut joined_stream = streams.join_map().await;
+    let mut okx_feed = streams
+        .select(ExchangeId::Okx)
+        .unwrap();
 
-    while let Some((exchange, data)) = joined_stream.next().await {
-        println!("Exchange: {exchange}, MarketEvent<DataKind>: {data:?}");
+    while let Some(trade) = okx_feed.recv().await {
+        println!("MarketEvent<PublicTrade>: {trade:?}");
     }
 }
