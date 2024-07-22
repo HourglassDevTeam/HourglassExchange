d830399edbbf98e73068b904f3f6196b07e7b31f
arthur <arthur@ciuyat.com>
2024-07-22 16:05:46 +0800

    finish optimising historical_stream.rs

--- src/common_skeleton/position.rs
+++ src/common_skeleton/position.rs
@@ -54,23 +54,19 @@ pub enum PositionKind
 }
 
 #[allow(dead_code)]
-#[derive(Clone, Debug)]
-pub struct PerpetualPosition
-{
-    token: String,                  // token字段，用于存储某种代币的名称，类型为String
-    pos_config: PerpetualPositionConfig, // pos_config字段，用于存储与SwapPosition相关的配置，SwapPositionConfig类型
-    pos_size: f64,                  // pos_size字段，表示头寸大小，类型为f64（64位浮点数）
-    average_price: f64,             // average_price字段，表示平均成交价格，类型为f64
-    liquidation_price: f64,         // liquidation_price字段，表示清算价格，类型为f64
-    margin: f64,                    // margin字段，表示保证金比例，类型为f64
-    pnl: f64,                       // pnl字段，表示未实现盈亏（Profit and Loss），类型为f64
-    fee: f64,                       // fee字段，表示交易费用，类型为f64
-    funding_fee: f64,               // funding_fee字段，表示资金费用，类型为f64
-    update_time_stamp: i64,         // update_time_stamp字段，表示最后更新时间的时间戳，类型为i64（64位整数）
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct PerpetualPosition {
+    pub base: PositionMeta,
+    pub pos_config: PerpetualPositionConfig,
+    pub liquidation_price: f64,
+    pub margin: f64,
+    pub fee: f64,
+    pub funding_fee: f64,
 }
+
 // NOTE 此处为一个尽量详细的Position案例
 #[derive(Clone, PartialEq, PartialOrd, Debug, Deserialize, Serialize)]
-pub struct Position {
+pub struct PositionMeta {
     pub position_id: String,
     pub enter_ts: i64,
     pub update_ts: i64,
