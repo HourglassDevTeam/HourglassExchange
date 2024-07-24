e0b1adcbcb3e446d0402ab584ce32149998babff
arthur <arthur@ciuyat.com>
2024-07-24 11:51:47 +0800

    revise order module

--- src/simulated_exchange/account/slippage_registry.rs
+++ src/simulated_exchange/account/slippage_registry.rs
@@ -1,30 +1,33 @@
-use crate::common_skeleton::{datafeed::event::MarketEvent, order::OrderId};
-use crate::common_skeleton::order::{Cancelled, Open, Order, RequestOpen};
-
+use crate::common_skeleton::order::{Order, RequestOpen};
+use crate::common_skeleton::order::OrderId;
 
 /// NOTE 在需要模拟延迟的回测场景下仅使用这种Pending状态。
 #[allow(dead_code)]
 pub struct SimulatedOpen
 {
+    pub processed_ts: i64,
     pub id: OrderId,
     pub price: f64,
     pub size: f64,
     pub filled_quantity: f64,
 }
 
-impl From<(OrderId, Order<RequestOpen>)> for Order<SimulatedOpen>
-{
-    fn from((id, request): (OrderId, Order<RequestOpen>)) -> Self
-    {
-        Self { kind: request.kind,
+impl From<(OrderId, i64, Order<RequestOpen>)> for Order<SimulatedOpen> {
+    fn from((id, exchange_ts, request): (OrderId, i64, Order<RequestOpen>)) -> Self {
+        Self {
+            kind: request.kind,
             exchange: request.exchange.clone(),
             instrument: request.instrument.clone(),
             cid: request.cid,
             client_ts: request.client_ts,
             side: request.side,
-            state: SimulatedOpen { id,
+            state: SimulatedOpen {
+                processed_ts: exchange_ts,
+                id,
                 price: request.state.price,
                 size: request.state.size,
-                filled_quantity: 0.0 } }
+                filled_quantity: 0.0,
+            },
+        }
     }
 }
