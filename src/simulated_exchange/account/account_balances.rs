ba1810cb8d39f06cb32cb0751b739f039ebbdb2a
arthur <arthur@ciuyat.com>
2024-07-26 16:22:54 +0800

    minor tweaks

--- src/simulated_exchange/account/account_balances.rs
+++ src/simulated_exchange/account/account_balances.rs
@@ -2,7 +2,7 @@ use std::{
     collections::HashMap,
     fmt::Debug,
     ops::{Deref, DerefMut},
-    sync::Arc,
+    sync::{Arc, Weak},
 };
 
 use tokio::sync::RwLock;
@@ -27,7 +27,7 @@ pub struct AccountBalances<Event>
     where Event: Clone + Send + Sync + Debug + 'static + Ord + Ord
 {
     pub balance_map: HashMap<Token, Balance>,
-    pub account_ref: Arc<RwLock<Account<Event>>>,
+    pub account_ref: Weak<RwLock<Account<Event>>>, // NOTE 如果不使用弱引用，可能会导致循环引用和内存泄漏。
 }
 
 impl<Event> PartialEq for AccountBalances<Event> where Event: Clone + Send + Sync + Debug + 'static + Ord
@@ -44,38 +44,51 @@ impl<Event> AccountBalances<Event> where Event: Clone + Send + Sync + Debug + 's
     /// 返回指定[`Token`]的[`Balance`]的引用。
     pub fn balance(&self, token: &Token) -> Result<&Balance, ExecutionError>
     {
-        self.get(token)
+        self.balance_map
+            .get(token)
             .ok_or_else(|| ExecutionError::Simulated(format!("SimulatedExchange is not configured for Token: {token}")))
     }
 
     /// 返回指定[`Token`]的[`Balance`]的可变引用。
     pub fn balance_mut(&mut self, token: &Token) -> Result<&mut Balance, ExecutionError>
     {
-        self.get_mut(token)
+        self.balance_map
+            .get_mut(token)
             .ok_or_else(|| ExecutionError::Simulated(format!("SimulatedExchange is not configured for Token: {token}")))
     }
 
+    /// Sets the account reference.
     pub fn set_account(&mut self, account: Arc<RwLock<Account<Event>>>)
     {
-        self.account_ref = account;
+        self.account_ref = Arc::downgrade(&account);
     }
 
     /// 获取指定 [`InstrumentKind`] 的手续费。
     pub async fn get_fee(&self, instrument_kind: &InstrumentKind) -> Result<f64, ExecutionError>
     {
-        let account_read = self.account_ref.read().await;
-        let config_read = account_read.config.read().await;
-        config_read.fees_book
-                   .get(instrument_kind)
-                   .cloned()
-                   .ok_or_else(|| ExecutionError::Simulated(format!("SimulatedExchange is not configured for InstrumentKind: {:?}", instrument_kind)))
+        if let Some(account) = self.account_ref.upgrade() {
+            let account_read = account.read().await;
+            let config_read = account_read.config.read().await;
+            config_read.fees_book
+                       .get(instrument_kind)
+                       .cloned()
+                       .ok_or_else(|| ExecutionError::Simulated(format!("SimulatedExchange is not configured for InstrumentKind: {:?}", instrument_kind)))
+        }
+        else {
+            Err(ExecutionError::Simulated("Account reference is not set".to_string()))
+        }
     }
 
     // 异步方法来获取 Account 的某个字段
     pub async fn get_exchange_ts(&self) -> Result<i64, ExecutionError>
     {
-        let account_read = self.account_ref.read().await;
-        Ok(account_read.exchange_timestamp)
+        if let Some(account) = self.account_ref.upgrade() {
+            let account_read = account.read().await;
+            Ok(account_read.exchange_timestamp)
+        }
+        else {
+            Err(ExecutionError::Simulated("Account reference is not set".to_string()))
+        }
     }
 
     /// 获取所有[`Token`]的[`Balance`]。
@@ -100,16 +113,14 @@ impl<Event> AccountBalances<Event> where Event: Clone + Send + Sync + Debug + 's
     /// [`Balance`]的变化取决于[`Order<Open>`]是[`Side::Buy`]还是[`Side::Sell`]。
     pub async fn update_from_open(&mut self, open: &Order<Open>, required_balance: f64) -> AccountEvent
     {
-        let _updated_balance = match open.side {
+        let updated_balance = match open.side {
             | Side::Buy => {
                 let balance = self.balance_mut(&open.instrument.quote).expect("[UniLink_Execution] : Balance existence is questionable");
-
                 balance.available -= required_balance;
                 TokenBalance::new(open.instrument.quote.clone(), *balance)
             }
             | Side::Sell => {
                 let balance = self.balance_mut(&open.instrument.base).expect("[UniLink_Execution] : Balance existence is questionable");
-
                 balance.available -= required_balance;
                 TokenBalance::new(open.instrument.base.clone(), *balance)
             }
@@ -117,7 +128,7 @@ impl<Event> AccountBalances<Event> where Event: Clone + Send + Sync + Debug + 's
 
         AccountEvent { exchange_timestamp: self.get_exchange_ts().await.expect("[UniLink_Execution] : Failed to get exchange timestamp"),
                        exchange: ExchangeVariant::Simulated,
-                       kind: AccountEventKind::Balance(_updated_balance) }
+                       kind: AccountEventKind::Balance(updated_balance) }
     }
 
     /// 当client取消[`Order<Open>`]时，更新相关的[`Token`] [`Balance`]。
@@ -128,14 +139,12 @@ impl<Event> AccountBalances<Event> where Event: Clone + Send + Sync + Debug + 's
             | Side::Buy => {
                 let balance = self.balance_mut(&cancelled.instrument.quote)
                                   .expect("[UniLink_Execution] : Balance existence checked when opening Order");
-
                 balance.available += cancelled.state.price * cancelled.state.remaining_quantity();
                 TokenBalance::new(cancelled.instrument.quote.clone(), *balance)
             }
             | Side::Sell => {
                 let balance = self.balance_mut(&cancelled.instrument.base)
                                   .expect("[UniLink_Execution] : Balance existence checked when opening Order");
-
                 balance.available += cancelled.state.remaining_quantity();
                 TokenBalance::new(cancelled.instrument.base.clone(), *balance)
             }
@@ -145,50 +154,34 @@ impl<Event> AccountBalances<Event> where Event: Clone + Send + Sync + Debug + 's
     /// 从交易中更新余额并返回 [`AccountEvent`]
     pub async fn update_from_trade(&mut self, market_event: &MarketEvent<ClickhouseTrade>) -> AccountEvent
     {
-        // let's start with a destructuring assignment
         let Instrument { base, quote, kind, .. } = &market_event.instrument;
-
-        // 获取手续费
         let fee = self.get_fee(kind).await.unwrap_or(0.0);
-        // 将 side 字符串转换为 Side 枚举
         let side = market_event.kind.parse_side();
-        // 计算 base 和 quote 余额的变化
         let (base_delta, quote_delta) = match side {
             | Side::Buy => {
-                // Base 的总余额和可用余额增加 trade.size 减去 base 的交易费用
                 let base_increase = market_event.kind.amount - fee;
                 let base_delta = BalanceDelta { total: base_increase,
                                                 available: base_increase };
-
-                // Quote 的总余额减少 (trade.size * price)
-                // 注意: 可用余额已在买单开单时减少
                 let quote_delta = BalanceDelta { total: -market_event.kind.amount * market_event.kind.price,
                                                  available: 0.0 };
-
                 (base_delta, quote_delta)
             }
             | Side::Sell => {
-                // Base 的总余额减少 trade.size
-                // 注意: 可用余额已在卖单开单时减少
                 let base_delta = BalanceDelta { total: -market_event.kind.amount,
                                                 available: 0.0 };
-
-                // Quote 的总余额和可用余额增加 (trade.size * price) 减去 quote 的交易费用
                 let quote_increase = (market_event.kind.amount * market_event.kind.price) - fee;
                 let quote_delta = BalanceDelta { total: quote_increase,
                                                  available: quote_increase };
-
                 (base_delta, quote_delta)
             }
         };
 
-        // 应用 BalanceDelta 并返回更新后的余额
-        let _base_balance = self.update(base, base_delta);
-        let _quote_balance = self.update(quote, quote_delta);
+        let base_balance = self.update(base, base_delta);
+        let quote_balance = self.update(quote, quote_delta);
 
         AccountEvent { exchange_timestamp: self.get_exchange_ts().await.expect("[UniLink_Execution] : Failed to get exchange timestamp"),
                        exchange: ExchangeVariant::Simulated,
-                       kind: AccountEventKind::Balances(vec![TokenBalance::new(base.clone(), _base_balance), TokenBalance::new(quote.clone(), _quote_balance),]) }
+                       kind: AccountEventKind::Balances(vec![TokenBalance::new(base.clone(), base_balance), TokenBalance::new(quote.clone(), quote_balance),]) }
     }
 
     /// 将 [`BalanceDelta`] 应用于指定 [`Token`] 的 [`Balance`]，并返回更新后的 [`Balance`] 。
@@ -223,37 +216,203 @@ impl<Event> DerefMut for AccountBalances<Event> where Event: Clone + Send + Sync
 #[cfg(test)]
 mod tests
 {
-    use super::*;
-    use crate::common_skeleton::order::OrderKind;
+    use std::sync::Arc;
 
-    #[test]
-    fn test_check_order_kind_support()
+    use tokio::sync::mpsc::unbounded_channel;
+    use uuid::Uuid;
+
+    use crate::{
+        common_skeleton::{
+            datafeed::event::MarketEvent,
+            event::ClientOrderId,
+            order::{OrderKind, OrderRole},
+        },
+        simulated_exchange::{
+            account::{
+                account_config::{AccountConfig, CommissionLevel, CommissionRates, MarginMode, PositionMode},
+                account_latency::{AccountLatency, FluctuationMode},
+                account_market_feed::AccountDataStreams,
+                account_orders::AccountOrders,
+                Account,
+            },
+            load_from_clickhouse::queries_operations::ClickhouseTrade,
+        },
+    };
+
+    use super::*;
+    #[allow(dead_code)]
+    async fn create_test_account() -> Arc<RwLock<Account<MarketEvent<ClickhouseTrade>>>>
     {
-        struct TestCase
+        let (account_event_tx, _account_event_rx) = unbounded_channel();
+        let (market_event_tx, _market_event_rx) = unbounded_channel();
+
+        let instruments = vec![]; // Populate with test data if needed
+        let account_latency = AccountLatency { fluctuation_mode: FluctuationMode::None,
+                                               maximum: 100,
+                                               minimum: 0,
+                                               current_value: 50 };
+
+        Arc::new(RwLock::new(Account { exchange_timestamp: 0,
+                                       data: Arc::new(RwLock::new(AccountDataStreams::new())),
+                                       account_event_tx,
+                                       market_event_tx,
+                                       config: Arc::new(RwLock::new(AccountConfig { margin_mode: MarginMode::SimpleMode,
+                                                                                    position_mode: PositionMode::LongShortMode,
+                                                                                    commission_level: CommissionLevel::Lv3,
+                                                                                    current_commission_rate: CommissionRates { spot_maker: 0.001,
+                                                                                                                               spot_taker: 0.002,
+                                                                                                                               perpetual_open: 0.001,
+                                                                                                                               perpetual_close: 0.002 },
+                                                                                    leverage_book: HashMap::new(),
+                                                                                    fees_book: HashMap::new() })),
+                                       balances: Arc::new(RwLock::new(AccountBalances { balance_map: HashMap::new(),
+                                                                                        account_ref: Weak::new() })),
+                                       positions: Arc::new(RwLock::new(Vec::new())),
+                                       orders: Arc::new(RwLock::new(AccountOrders::new(instruments, account_latency).await)) }))
+    }
+
+    #[tokio::test]
+    async fn test_balance()
+    {
+        let token = Token::new("BTC");
+        let balance = Balance::new(100.0, 100.0);
+        let mut balance_map = HashMap::new();
+        balance_map.insert(token.clone(), balance);
+
+        let account = create_test_account().await;
+        let account_ref = Arc::downgrade(&account);
+
+        let balances = AccountBalances { balance_map, account_ref };
+
+        assert_eq!(balances.balance(&token).unwrap().available, 100.0);
+    }
+
+    #[tokio::test]
+    async fn test_balance_mut()
+    {
+        let token = Token::new("BTC");
+        let balance = Balance::new(100.0, 100.0);
+        let mut balance_map = HashMap::new();
+        balance_map.insert(token.clone(), balance);
+
+        let account = create_test_account().await;
+        let account_ref = Arc::downgrade(&account);
+
+        let mut balances = AccountBalances { balance_map, account_ref };
+
         {
-            kind: OrderKind,
-            expected: Result<(), ExecutionError>,
+            let balance_mut = balances.balance_mut(&token).unwrap();
+            balance_mut.available = 50.0;
         }
 
-        let tests = vec![TestCase { // TC0: Market
-                                    kind: OrderKind::Market,
-                                    expected: Ok(()) },
-                         TestCase { // TC1: Limit
-                                    kind: OrderKind::Limit,
-                                    expected: Ok(()) },
-                         TestCase { // TC2: PostOnly
-                                    kind: OrderKind::PostOnly,
-                                    expected: Ok(()) },
-                         TestCase { // TC3: Immediate Or Cancel
-                                    kind: OrderKind::ImmediateOrCancel,
-                                    expected: Ok(()) },];
+        assert_eq!(balances.balance(&token).unwrap().available, 50.0);
+    }
 
-        for (index, test) in tests.into_iter().enumerate() {
-            let actual = Account::<()>::order_validity_check(test.kind); // Specify the generic type `()`
-            match test.expected {
-                | Ok(()) => assert!(actual.is_ok(), "TC{} is good", index),
-                | Err(_) => assert!(actual.is_err(), "TC{} failed", index),
-            }
+    #[tokio::test]
+    async fn test_get_fee()
+    {
+        let instrument_kind = InstrumentKind::Spot;
+        let mut fees_book = HashMap::new();
+        fees_book.insert(instrument_kind.clone(), 0.1);
+
+        let account = create_test_account().await;
+        account.write().await.config.write().await.fees_book = fees_book;
+
+        let account_ref = Arc::downgrade(&account);
+
+        let balances = AccountBalances { balance_map: HashMap::new(),
+                                         account_ref };
+
+        let fee = balances.get_fee(&instrument_kind).await.unwrap();
+        assert_eq!(fee, 0.1);
+    }
+
+    #[tokio::test]
+    async fn test_get_exchange_ts()
+    {
+        let account = create_test_account().await;
+        account.write().await.exchange_timestamp = 1627843987;
+
+        let account_ref = Arc::downgrade(&account);
+
+        let balances = AccountBalances { balance_map: HashMap::new(),
+                                         account_ref };
+
+        let exchange_ts = balances.get_exchange_ts().await.unwrap();
+        assert_eq!(exchange_ts, 1627843987);
+    }
+
+    #[tokio::test]
+    async fn test_fetch_all()
+    {
+        let token = Token::new("BTC");
+        let balance = Balance::new(100.0, 100.0);
+        let mut balance_map = HashMap::new();
+        balance_map.insert(token.clone(), balance);
+
+        let account = create_test_account().await;
+        let account_ref = Arc::downgrade(&account);
+
+        let balances = AccountBalances { balance_map, account_ref };
+
+        let all_balances = balances.fetch_all();
+        assert_eq!(all_balances.len(), 1);
+        assert_eq!(all_balances[0].balance.available, 100.0);
+    }
+
+    #[tokio::test]
+    async fn test_has_sufficient_available_balance()
+    {
+        let token = Token::new("BTC");
+        let balance = Balance::new(100.0, 100.0);
+        let mut balance_map = HashMap::new();
+        balance_map.insert(token.clone(), balance);
+
+        let account = create_test_account().await;
+        let account_ref = Arc::downgrade(&account);
+
+        let balances = AccountBalances { balance_map, account_ref };
+
+        assert!(balances.has_sufficient_available_balance(&token, 50.0).is_ok());
+        assert!(balances.has_sufficient_available_balance(&token, 150.0).is_err());
+    }
+
+    #[tokio::test]
+    async fn test_update_from_open()
+    {
+        let token = Token::new("BTC");
+        let balance = Balance::new(100.0, 100.0);
+        let mut balance_map = HashMap::new();
+        balance_map.insert(token.clone(), balance);
+
+        let account = create_test_account().await;
+        let account_ref = Arc::downgrade(&account);
+
+        let mut balances = AccountBalances { balance_map, account_ref };
+
+        let instrument = Instrument::new(token.clone(), token.clone(), InstrumentKind::Spot);
+        let client_order_id = Uuid::new_v4();
+        let open_state = Open { id: client_order_id.into(),
+                                price: 50000.0,
+                                size: 1.0,
+                                filled_quantity: 0.0,
+                                order_role: OrderRole::Maker,
+                                received_ts: 0 };
+        let order = Order { kind: OrderKind::Limit,
+                            exchange: ExchangeVariant::Simulated,
+                            instrument: instrument.clone(),
+                            client_ts: 0,
+                            cid: ClientOrderId(client_order_id.clone()),
+                            side: Side::Buy,
+                            state: open_state };
+        let account_event = balances.update_from_open(&order, 50.0).await;
+
+        assert_eq!(balances.balance(&token).unwrap().available, 50.0);
+        if let AccountEventKind::Balance(token_balance) = account_event.kind {
+            assert_eq!(token_balance.balance.available, 50.0);
+        }
+        else {
+            panic!("Unexpected account event kind");
         }
     }
 }
