8c24ec85b67f1ce5d9c3136f9bc72a72bdff5ec5
arthur <arthur@ciuyat.com>
2024-08-15 17:54:47 +0800

    add an example for  data streaming

---
+++ examples/CH_query_union_by_row.rs
@@ -0,0 +1,35 @@
+use std::time::Instant;
+use unilink_execution::sandbox::clickhouse_api::queries_operations::ClickHouseClient;
+
+#[tokio::main]
+async fn main() {
+    let client = ClickHouseClient::new();
+    let exchange = "binance";
+    let instrument = "futures";
+    let date = "2024_05_05";
+    let base = "1000RATS";
+    let quote = "USDT";
+
+    // EXAMPLE 1 MANUALLY BUILD THE QUERY
+    // let database_name = client.construct_database_name(exchange, instrument, "trades");
+    // let table_name = client.construct_table_name(exchange, instrument, "trades", date, base, quote);
+    // let query = ClickHouseQueryBuilder::new()
+    //     .select("symbol, side, price, timestamp, amount")
+    //     .from( &database_name, &table_name)
+    //     .order("timestamp", Some("DESC"))
+    //     .build();
+    // println!("[UniLinkExecution] : Constructed query {}", query);
+    //
+    // let client_ref = client.client.read().await;
+    // let mut cursor = client_ref.query(&query).fetch::<ClickhousePublicTrade>().unwrap();
+
+
+    // EXAMPLE 2 USE PREDEFINED METHOD
+    let mut cursor = client.cursor_unioned_public_trades(exchange,instrument,date).await.unwrap();
+    let start_time = Instant::now();
+    while let Ok(Some(row)) = cursor.next().await {
+        println!("{:?}", row)
+    }
+    let duration = start_time.elapsed();
+    println!("ClickhousePublicTrade data fetched in: {:?}", duration);
+}
