840f96e97a812a5e06f6507aae6f4d61e7cb37dc
arthur <arthur@ciuyat.com>
2024-08-22 15:59:51 +0800

    add tests for account states

--- src/sandbox/account/account_config.rs
+++ src/sandbox/account/account_config.rs
@@ -171,55 +171,55 @@ pub enum CommissionLevel
 }
 
 // NOTE : this initiator is currently not useful as we now import config from config.
-// pub struct AccountConfigInitiator
-// {
-//     margin_mode: Option<MarginMode>,
-//     position_mode: Option<PositionDirectionMode>,
-//     position_margin_mode: Option<PositionMarginMode>,
-//     commission_level: Option<CommissionLevel>,
-// }
-//
-// impl AccountConfigInitiator
-// {
-//     pub fn new() -> Self
-//     {
-//         Self { margin_mode: None,
-//                position_mode: None,
-//                position_margin_mode: None,
-//                commission_level: None }
-//     }
-//
-//     pub fn margin_mode(mut self, margin_mode: MarginMode) -> Self
-//     {
-//         self.margin_mode = Some(margin_mode);
-//         self
-//     }
-//
-//     pub fn position_mode(mut self, position_mode: PositionDirectionMode) -> Self
-//     {
-//         self.position_mode = Some(position_mode);
-//         self
-//     }
-//
-//     pub fn commission_level(mut self, commission_level: CommissionLevel) -> Self
-//     {
-//         self.commission_level = Some(commission_level);
-//         self
-//     }
-//
-//     // NOTE CommissionRates here are initiated as all zeros. this could be replaced with actual values fetched from the exchange,
-//     //      be it sandbox or not.
-//     pub fn initiate(self) -> Result<AccountConfig, &'static str>
-//     {
-//         Ok(AccountConfig { margin_mode: self.margin_mode.ok_or("margin_mode is required")?,
-//                            position_mode: self.position_mode.ok_or("position_mode is required")?,
-//                            position_margin_mode: self.position_margin_mode.ok_or("position_mode is required")?,
-//                            commission_level: self.commission_level.ok_or("commission_level is required")?,
-//                            current_commission_rate: CommissionRates { spot_maker: 0.0,
-//                                                                       spot_taker: 0.0,
-//                                                                       perpetual_open: 0.0,
-//                                                                       perpetual_close: 0.0 },
-//                            leverage_book: Default::default(),
-//                            fees_book: Default::default() })
-//     }
-// }
+pub struct AccountConfigInitiator
+{
+    margin_mode: Option<MarginMode>,
+    position_mode: Option<PositionDirectionMode>,
+    position_margin_mode: Option<PositionMarginMode>,
+    commission_level: Option<CommissionLevel>,
+}
+
+impl AccountConfigInitiator
+{
+    pub fn new() -> Self
+    {
+        Self { margin_mode: None,
+               position_mode: None,
+               position_margin_mode: None,
+               commission_level: None }
+    }
+
+    pub fn margin_mode(mut self, margin_mode: MarginMode) -> Self
+    {
+        self.margin_mode = Some(margin_mode);
+        self
+    }
+
+    pub fn position_mode(mut self, position_mode: PositionDirectionMode) -> Self
+    {
+        self.position_mode = Some(position_mode);
+        self
+    }
+
+    pub fn commission_level(mut self, commission_level: CommissionLevel) -> Self
+    {
+        self.commission_level = Some(commission_level);
+        self
+    }
+
+    // NOTE CommissionRates here are initiated as all zeros. this could be replaced with actual values fetched from the exchange,
+    //      be it sandbox or not.
+    pub fn initiate(self) -> Result<AccountConfig, &'static str>
+    {
+        Ok(AccountConfig { margin_mode: self.margin_mode.ok_or("margin_mode is required")?,
+                           position_mode: self.position_mode.ok_or("position_mode is required")?,
+                           position_margin_mode: self.position_margin_mode.ok_or("position_mode is required")?,
+                           commission_level: self.commission_level.ok_or("commission_level is required")?,
+                           current_commission_rate: CommissionRates { spot_maker: 0.0,
+                                                                      spot_taker: 0.0,
+                                                                      perpetual_open: 0.0,
+                                                                      perpetual_close: 0.0 },
+                           leverage_book: Default::default(),
+                           fees_book: Default::default() })
+    }
+}
