21d340c0ae74f3a3ad54b8bb651062ae3b5f8791
arthur <arthur@ciuyat.com>
2024-08-23 12:53:39 +0800

    add tests for account states

--- examples/CH_create_unioned_tables_between_dates_binance.rs
+++ examples/CH_create_unioned_tables_between_dates_binance.rs
@@ -1,14 +1,32 @@
 use chrono::{Duration, NaiveDate};
+#[cfg(feature = "lark")]
+use dotenvy::dotenv;
+#[cfg(feature = "lark")]
+use std::env;
 use rayon::prelude::*;
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
 };
+#[cfg(feature = "lark")]
+use open_lark::{custom_bot::CustomBot, service::im::v1::message::MessageText};
 use unilink_execution::sandbox::{clickhouse_api::queries_operations::ClickHouseClient, utils::chrono_operations::extract_date};
 
 #[tokio::main]
-async fn main()
-{
+async fn main() {
+    // 检测是否为 release 模式
+    #[cfg(debug_assertions)]
+    println!("[UniLinkExecution] : Running in debug mode");
+
+    #[cfg(not(debug_assertions))]
+    println!("[UniLinkExecution] : Running in release mode");
+
+    #[cfg(feature = "lark")]
+    {
+        // 加载 .env 文件
+        dotenv().expect(".env file not found");
+    }
+
     // 创建 ClickHouse 客户端实例
     let client = Arc::new(ClickHouseClient::new());
 
@@ -26,25 +44,32 @@ async fn main()
     // 创建一个表名与日期的字典，并将其转换为 Arc<Mutex<_>> 以供并行使用
     let table_date_map: Arc<Mutex<HashMap<String, NaiveDate>>> =
         Arc::new(Mutex::new(table_names.par_iter()
-                                       .filter_map(|table_name| {
-                                           if !table_name.contains("union") {
-                                               if let Some(table_date_str) = extract_date(table_name) {
-                                                   if let Ok(table_date) = NaiveDate::parse_from_str(&table_date_str, "%Y_%m_%d") {
-                                                       return Some((table_name.clone(), table_date));
-                                                   }
-                                               }
-                                           }
-                                           None
-                                       })
-                                       .collect()));
+            .filter_map(|table_name| {
+                if !table_name.contains("union") {
+                    if let Some(table_date_str) = extract_date(table_name) {
+                        if let Ok(table_date) = NaiveDate::parse_from_str(&table_date_str, "%Y_%m_%d") {
+                            return Some((table_name.clone(), table_date));
+                        }
+                    }
+                }
+                None
+            })
+            .collect()));
 
     // 计算总的表数量，用于进度汇报
     let total_tables = table_date_map.lock()
-                                     .unwrap()
-                                     .values()
-                                     .filter(|&&table_date| table_date >= start_date && table_date <= end_date)
-                                     .count();
+        .unwrap()
+        .values()
+        .filter(|&&table_date| table_date >= start_date && table_date <= end_date)
+        .count();
     let mut processed_tables = 0;
+
+    #[cfg(feature = "lark")]
+    let bot = {
+        let hook_url = env::var("HOOK_URL").unwrap();
+        let secret = env::var("HOOK_SECRET").ok();
+        CustomBot::new(hook_url, secret.as_deref())
+    };
 
     // 遍历日期范围
     let mut current_date = start_date;
@@ -56,9 +81,9 @@ async fn main()
             let table_date_map = table_date_map.clone();
             let map = table_date_map.lock().unwrap();
             map.par_iter()
-               .filter(|&(_, &table_date)| table_date == current_date)
-               .map(|(table_name, _)| table_name.clone())
-               .collect()
+                .filter(|&(_, &table_date)| table_date == current_date)
+                .map(|(table_name, _)| table_name.clone())
+                .collect()
         };
 
         // 并行创建联合表
@@ -82,7 +107,7 @@ async fn main()
                     | Err(e) => eprintln!("[UniLinkExecution] : Error creating table: {}", e),
                 }
             }).await
-              .unwrap();
+                .unwrap();
 
             // 更新进度
             processed_tables += tables.len();
@@ -98,5 +123,17 @@ async fn main()
         current_date += Duration::days(1);
     }
 
-    println!("[UniLinkExecution] : Union tables creation is done for {} tables", total_tables);
+    #[cfg(feature = "lark")]
+    {
+        let message = MessageText::new(&format!(
+            "[UniLinkExecution] : Union tables creation is done for {} tables",
+            total_tables
+        ));
+        bot.send_message(message).await.unwrap();
+    }
+
+    println!(
+        "[UniLinkExecution] : Union tables creation is done for {} tables",
+        total_tables
+    );
 }
