7f2067b9b9e425f4e910767d1af86f13f8650839
arthur <arthur@ciuyat.com>
2024-08-24 10:03:03 +0800

    add tests for account states

--- examples/CH_insert_between_dates.rs
+++ examples/CH_insert_between_dates.rs
@@ -16,8 +16,6 @@ async fn main() {
 
     // 在循环外部获取所有表名
     let database = format!("{}_{}_{}", exchange, instrument, channel);
-    // let start_date = NaiveDate::from_ymd_opt(2019, 11, 19).expect("Invalid start date");
-    // let end_date = NaiveDate::from_ymd_opt(2024, 8, 10).expect("Invalid end date");
     let start_date = NaiveDate::from_ymd_opt(2024, 5, 5).expect("Invalid start date");
     let end_date = NaiveDate::from_ymd_opt(2024, 5, 5).expect("Invalid end date");
 
@@ -44,17 +42,21 @@ async fn main() {
             .cloned()
             .collect();
 
-        println!("\n[NOTE] : The filtered_table_names to be added into the target table are:");
 
-        for table_name in &filtered_table_names {
-            println!("- {}", table_name);
-        }
-
-        println!("\n[NOTE] : End of the list.\n");
 
         // 如果有符合的表，调用 insert_into_unioned_table 方法
         if !filtered_table_names.is_empty() {
+
+            println!("\n[NOTE] : The filtered_table_names to be added into the target table are:");
+
+            for table_name in &filtered_table_names {
+                println!("- {}", table_name);
+            }
+
+            println!("\n[NOTE] : End of the list.\n");
             client.insert_into_unioned_table(&database, &target_table_name, &filtered_table_names, true).await.unwrap();
             current_date += Duration::days(1);
-    }
+    }else {
+            println!("[UniLinkExecution] : No tables found for the date: {}", current_date);
+        }
 }}
