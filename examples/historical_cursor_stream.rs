b7befc6d270dc3737c39c13445a32dd67e23bdb6
arthur <arthur@ciuyat.com>
2024-08-24 11:34:27 +0800

    add tests for account states

--- examples/historical_cursor_stream.rs
+++ examples/historical_cursor_stream.rs
@@ -5,10 +5,8 @@ use tokio::{sync::mpsc, time::timeout};
 
 #[tokio::main]
 async fn main() {
-    println!("Attempting to connect to ClickHouse...");
     // 创建 ClickHouse 客户端实例
     let client = Arc::new(ClickHouseClient::new());
-    println!("Client created successfully. Attempting to fetch data...");
 
     // 定义参数
     let exchange = "binance";
@@ -24,7 +22,6 @@ async fn main() {
         Ok(mut cursor) => {
             // 创建通道
             let (tx, rx) = mpsc::unbounded_channel();
-            println!("Cursor created successfully");
 
             // 启动一个任务来从游标读取数据并发送到通道
             let cursor_task = tokio::spawn(async move {
@@ -34,20 +31,20 @@ async fn main() {
                             // println!("{:?}", &trade);
                             if tx.send(trade).is_err() {
                                 // 如果发送失败（例如接收者已关闭），退出循环
-                                eprintln!("Failed to send trade, receiver might be closed.");
+                                eprintln!("[UniLinkExecution] : Failed to send trade, receiver might be closed.");
                                 break;
                             }
                         }
                         Ok(Ok(None)) => {
-                            println!("No more data to read from the cursor.");
+                            println!("[UniLinkExecution] : Cursor data processing for date {} is complete.", date);
                             break;
                         }
                         Ok(Err(e)) => {
-                            eprintln!("Error while reading cursor: {:?}", e);
+                            eprintln!("[UniLinkExecution] : Error while reading cursor: {:?}", e);
                             break;
                         }
                         Err(_) => {
-                            eprintln!("Timeout while reading cursor");
+                            eprintln!("[UniLinkExecution] : Timeout while reading cursor");
                             break;
                         }
                     }
@@ -62,13 +59,13 @@ async fn main() {
 
             // 这里你可以选择等待 `cursor_task` 完成，或在其他地方进行更多操作
             if let Err(e) = cursor_task.await {
-                eprintln!("Cursor task was aborted: {:?}", e);
+                eprintln!("[UniLinkExecution] : Cursor task was aborted: {:?}", e);
             }
 
             // 你可以继续使用 data_streams 进行进一步的操作，例如合并、排序等
         }
         Err(e) => {
-            eprintln!("Error fetching trades: {:?}", e);
+            eprintln!("[UniLinkExecution] : Error fetching trades: {:?}", e);
         }
     }
 }
