8b8ad0d28cbf288b5e008a28336792391e04043e
arthur <arthur@ciuyat.com>
2024-08-26 20:43:38 +0800

    ready to remove generic type bound for Account

--- src/common_infrastructure/datafeed/mod.rs
+++ src/common_infrastructure/datafeed/mod.rs
@@ -1,15 +1,1 @@
-// /// NOTE currently under construction
-// use serde::{Deserialize, Serialize};
-//
 pub mod public_event;
-pub mod historical;
-// pub mod historical;
-// pub mod live;
-//
-// pub trait MarketFeedDistributor
-// {
-//     // NOTE 历史市场事件迭代器和直接市场事件迭代器都会实现 MarketFeedDistributor，
-//     //      但是方式不同。所以此处不定义细节。
-//     fn fetch_next(&mut self) -> FeedStatus;
-// }
-//
