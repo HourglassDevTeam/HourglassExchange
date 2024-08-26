bb8a50c1b53bda8780562bc35c19e126a14cdda7
arthur <arthur@ciuyat.com>
2024-08-26 19:54:20 +0800

    ready to remove generic type bound for Account

--- src/common_infrastructure/datafeed/mod.rs
+++ src/common_infrastructure/datafeed/mod.rs
@@ -1,7 +1,7 @@
 // /// NOTE currently under construction
 // use serde::{Deserialize, Serialize};
 //
-// pub mod public_event;
+pub mod public_event;
 // pub mod historical;
 // pub mod live;
 //
@@ -12,12 +12,3 @@
 //     fn fetch_next(&mut self) -> FeedStatus;
 // }
 //
-// // #[derive(Clone, Eq, PartialEq, PartialOrd, Debug, Deserialize, Serialize)]
-// // pub enum FeedStatus
-// // {
-// //     Next(Event),
-// //     Unhealthy, // Unhealthy 变体，表示市场数据流状态不健康
-// //     Finished,  // Finished 变体，表示市场数据流已经结束
-// // }
-
-pub mod public_event;
