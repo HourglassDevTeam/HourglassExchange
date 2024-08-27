82be6a075103eeaae0cc698183a786e6e9442c30
arthur <arthur@ciuyat.com>
2024-08-27 20:03:20 +0800

    add exchange column to MarketTrade and union table's building query logic

--- src/sandbox/account/account_orders.rs
+++ src/sandbox/account/account_orders.rs
@@ -27,6 +27,37 @@ pub struct  AccountOrders
 impl AccountOrders
 {
     /// 从给定的 [`Instrument`] 列表选择构造一个新的 [`AccountOrders`]。
+    /// 创建一个新的 [`AccountOrders`] 实例。
+    ///
+    /// 该函数接受一组预先定义的金融工具（`Instrument`）和一个账户延迟生成器（`AccountLatency`），
+    /// 并返回一个初始化的 `AccountOrders` 实例，用于管理这些金融工具的订单和延迟模拟。
+    ///
+    /// # 参数
+    ///
+    /// * `instruments` - 一个 `Vec<Instrument>` 类型的列表，包含所有需要管理的金融工具。
+    /// * `account_latency` - 一个 `AccountLatency` 实例，用于生成和管理请求延迟的波动情况。
+    ///
+    /// # 返回值
+    ///
+    /// 返回一个初始化好的 `AccountOrders` 实例，其中包含了给定的金融工具、
+    /// 生成的延迟值数组，以及用于管理挂单的相关结构。
+    ///
+    /// # 示例
+    ///
+    /// ```rust
+    /// use unilink_execution::common_infrastructure::instrument::Instrument;
+    /// use unilink_execution::common_infrastructure::instrument::kind::InstrumentKind;
+    /// use unilink_execution::sandbox::account::account_latency::{AccountLatency, FluctuationMode};
+    /// use unilink_execution::sandbox::account::account_orders::AccountOrders;
+    ///
+    /// #[tokio::main]
+    /// async fn main() {
+    ///     let instruments = vec![Instrument::new("BTC", "USD", InstrumentKind::Spot)];
+    ///     let account_latency = AccountLatency::new(FluctuationMode::Sine, 100, 10);
+    ///     let account_orders = AccountOrders::new(instruments, account_latency).await;
+    ///     println!("新建的 AccountOrders 实例: {:?}", account_orders);
+    /// }
+    /// ```
     pub async fn new(instruments: Vec<Instrument>, mut account_latency: AccountLatency) -> Self
     {
         let selectable_latencies = Self::generate_latencies(&mut account_latency).await;
@@ -38,6 +69,18 @@ impl AccountOrders
                selectable_latencies }
     }
 
+
+    /// 生成一组预定义的延迟值数组，用于模拟订单延迟。
+    ///
+    /// 该函数通过调用 `fluctuate_latency` 函数来动态调整延迟值，并将结果存储在一个数组中。
+    ///
+    /// # 参数
+    ///
+    /// * `latency_generator` - 一个可变引用，指向 `AccountLatency` 实例，用于生成和调整延迟值。
+    ///
+    /// # 返回值
+    ///
+    /// 返回一个包含 20 个延迟值的数组 `[i64; 20]`，每个延迟值是通过 `AccountLatency` 计算得到的。
     async fn generate_latencies(latency_generator: &mut AccountLatency) -> [i64; 20]
     {
         let mut latencies = [0; 20];
@@ -48,6 +91,15 @@ impl AccountOrders
         latencies
     }
 
+    /// 从预定义的延迟值数组中随机选择一个延迟值。
+    ///
+    /// 该函数用于从 `selectable_latencies` 数组中随机选择一个延迟值，
+    /// 用于模拟不同请求的延迟情况，增强测试或模拟的真实性。
+    ///
+    /// # 返回值
+    ///
+    /// 返回一个随机选择的延迟值 `i64`。
+    ///
     fn get_random_latency(&self) -> i64
     {
         let mut rng = rand::thread_rng();
@@ -65,6 +117,19 @@ impl AccountOrders
     }
 
     /// 为每个 [`Instrument`] 获取出价和要价 [`Order<Open>`]。
+    ///
+    /// 该函数在以下情况下会被使用:
+    ///
+    /// 1. **查询订单状态**: 用户或系统需要查询当前账户的所有挂单，例如在界面上显示当前的挂单状态，或在数据分析时，了解有哪些订单还未成交。
+    /// 2. **取消所有挂单**: 在需要一次性取消所有挂单的情况下，首先可以通过 `fetch_all()` 获取所有挂单的列表，然后逐一取消这些订单。
+    /// 3. **定期检查或清理**: 系统可能会定期检查账户的挂单情况，确保所有挂单都在合理状态，或者在清理过程中使用该函数获取需要清理的订单。
+    /// 4. **系统恢复或重启后重建状态**: 如果交易系统因某种原因重启，系统可能需要重建账户的内部状态，此时 `fetch_all()` 可以用于获取所有挂单，以便在内存中重新建立订单簿的状态。
+    /// 5. **监控和日志记录**: 在监控或日志记录系统中，记录当前账户所有挂单的状态，有助于在出问题时追踪系统中未成交订单的详细信息。
+    ///
+    /// # 返回值
+    ///
+    /// 返回一个包含所有未完成订单的 `Vec<Order<Open>>`。
+    ///
     pub fn fetch_all(&self) -> Vec<Order<Open>>
     {
         self.instrument_orders_map
@@ -76,7 +141,16 @@ impl AccountOrders
             .collect()
     }
 
-    // 从PendingRegistry中删除订单的函数
+    /// 从 `pending_registry` 中删除指定的订单。
+    ///
+    /// # 参数
+    ///
+    /// - `order_id`: 要删除的订单的 `ClientOrderId`。
+    ///
+    /// # 返回值
+    ///
+    /// - 如果删除成功，返回 `Ok(())`。
+    /// - 如果未找到订单，返回 `Err(ExecutionError::OrderNotFound)`。
     pub fn remove_order_from_pending_registry(&mut self, order_id: ClientOrderId) -> Result<(), ExecutionError>
     {
         if self.pending_registry.remove(&order_id).is_some() {
@@ -87,6 +161,17 @@ impl AccountOrders
         }
     }
 
+
+    /// 将请求转换为一个待处理的订单 (`Pending`)。
+    ///
+    /// # 参数
+    ///
+    /// - `order`: 要处理的订单请求 (`Order<RequestOpen>`)。
+    ///
+    /// # 返回值
+    ///
+    /// - 返回一个包含预测时间戳的待处理订单 (`Order<Pending>`)。
+    ///
     pub async fn process_request_as_pending(&mut self, order: Order<RequestOpen>) -> Order<Pending>
     {
         // turn the request into an pending order with a predicted timestamp
@@ -104,6 +189,17 @@ impl AccountOrders
                                  predicted_ts: adjusted_client_ts } }
     }
 
+    /// 将请求注册为待处理订单。
+    ///
+    /// # 参数
+    ///
+    /// - `request`: 要注册的订单请求 (`Order<RequestOpen>`)。
+    ///
+    /// # 返回值
+    ///
+    /// - 如果订单成功注册，返回 `Ok(())`。
+    /// - 如果订单已存在，返回 `Err(ExecutionError::OrderAlreadyExists)`。
+    ///
     pub async fn register_pending_order(&mut self, request: Order<RequestOpen>) -> Result<(), ExecutionError>
     {
         if self.pending_registry.contains_key(&request.client_order_id) {
@@ -114,8 +210,25 @@ impl AccountOrders
         Ok(())
     }
 
-    // 判断是Maker还是Taker单
-    /// 确定订单是 Maker 还是 Taker
+    /// 根据订单类型和当前市场价格，确定订单是 Maker 还是 Taker。
+    ///
+    /// # 参数
+    ///
+    /// - `order`: 待处理的订单 (`Order<Pending>`)。
+    /// - `current_price`: 当前市场价格。
+    ///
+    /// # 返回值
+    ///
+    /// - 返回 `Ok(OrderRole::Maker)` 或 `Ok(OrderRole::Taker)`，分别表示订单是 Maker 或 Taker。
+    /// - 如果订单类型无法判断，返回 `Err(ExecutionError)`。
+    ///
+    /// # 逻辑
+    ///
+    /// - 对于 `Market` 类型的订单，总是返回 `OrderRole::Taker`，因为`Market`订单总是`Taker`订单。
+    /// - 对于 `Limit` 类型的订单，调用 `determine_limit_order_role` 来确定订单角色。
+    /// - 对于 `PostOnly` 类型的订单，调用 `determine_post_only_order_role` 来判断订单是否能作为 Maker，否则拒绝该订单。
+    /// - 对于 `ImmediateOrCancel` 和 `FillOrKill` 类型的订单，总是返回 `OrderRole::Taker`，因为这些订单需要立即成交。
+    /// - 对于 `GoodTilCancelled` 类型的订单，按照限价订单的逻辑来判断角色。
     pub fn determine_maker_taker(&mut self, order: &Order<Pending>, current_price: f64) -> Result<OrderRole, ExecutionError>
     {
         match order.kind {
@@ -131,7 +244,28 @@ impl AccountOrders
         }
     }
 
-    /// 判断限价订单是 Maker 还是 Taker
+    /// 根据限价订单的价格和当前市场价格，确定订单是 Maker 还是 Taker。
+    ///
+    /// # 参数
+    ///
+    /// - `order`: 待处理的限价订单 (`Order<Pending>`)。
+    /// - `current_price`: 当前市场价格。
+    ///
+    /// # 返回值
+    ///
+    /// - `Ok(OrderRole::Maker)`: 如果订单价格与当前市场价格相比，具有优势（买单价格高于或等于市场价格，或卖单价格低于或等于市场价格），则订单作为 Maker 角色。
+    /// - `Ok(OrderRole::Taker)`: 如果订单价格与当前市场价格相比，处于劣势（买单价格低于市场价格，或卖单价格高于市场价格），则订单作为 Taker 角色。
+    ///
+    /// # 逻辑
+    ///
+    /// - 对于买单 (`Side::Buy`):
+    ///   - 如果订单价格 (`order.state.price`) 大于或等于当前市场价格 (`current_price`)，则返回 `OrderRole::Maker`。
+    ///   - 否则，返回 `OrderRole::Taker`。
+    ///
+    /// - 对于卖单 (`Side::Sell`):
+    ///   - 如果订单价格 (`order.state.price`) 小于或等于当前市场价格 (`current_price`)，则返回 `OrderRole::Maker`。
+    ///   - 否则，返回 `OrderRole::Taker`。
+    ///
     fn determine_limit_order_role(&self, order: &Order<Pending>, current_price: f64) -> Result<OrderRole, ExecutionError>
     {
         match order.side {
@@ -154,7 +288,33 @@ impl AccountOrders
         }
     }
 
-    /// 判断 PostOnly 订单是否符合条件，如果不符合则删除并返回错误
+    /// 判断 PostOnly 订单是否符合条件，并确定其是 Maker 还是被拒绝。
+    ///
+    /// 如果订单不符合 PostOnly 的条件（即买单价格低于当前市场价格，或卖单价格高于当前市场价格），
+    /// 则会拒绝该订单，并将其从待处理订单中删除。
+    ///
+    /// # 参数
+    ///
+    /// - `order`: 待处理的 PostOnly 订单 (`Order<Pending>`)。
+    /// - `current_price`: 当前市场价格。
+    ///
+    /// # 返回值
+    ///
+    /// - `Ok(OrderRole::Maker)`: 如果订单价格符合 PostOnly 的条件，
+    ///   即买单价格高于或等于市场价格，或者卖单价格低于或等于市场价格，则订单作为 Maker 角色。
+    /// - `Err(ExecutionError::OrderRejected)`: 如果订单价格不符合 PostOnly 的条件，
+    ///   即买单价格低于市场价格，或者卖单价格高于市场价格，则订单会被拒绝并从待处理订单中删除。
+    ///
+    /// # 逻辑
+    ///
+    /// - 对于买单 (`Side::Buy`):
+    ///   - 如果订单价格 (`order.state.price`) 大于或等于当前市场价格 (`current_price`)，则返回 `OrderRole::Maker`。
+    ///   - 否则，调用 `self.reject_post_only_order(order)` 拒绝订单，并返回错误。
+    ///
+    /// - 对于卖单 (`Side::Sell`):
+    ///   - 如果订单价格 (`order.state.price`) 小于或等于当前市场价格 (`current_price`)，则返回 `OrderRole::Maker`。
+    ///   - 否则，调用 `self.reject_post_only_order(order)` 拒绝订单，并返回错误。
+    ///
     fn determine_post_only_order_role(&mut self, order: &Order<Pending>, current_price: f64) -> Result<OrderRole, ExecutionError>
     {
         match order.side {
@@ -177,7 +337,17 @@ impl AccountOrders
         }
     }
 
-    /// 拒绝不符合条件的 PostOnly 订单并移除
+    /// 拒绝不符合条件的 PostOnly 订单，并将其从待处理订单注册表中移除。
+    ///
+    /// 当一个 PostOnly 订单的价格不符合条件时（例如，买单价格低于市场价格，或卖单价格高于市场价格），
+    /// 该函数将拒绝此订单，并将其从 `pending_registry` 中删除。
+    ///
+    /// # 参数
+    ///
+    /// - `order`: 待拒绝的 PostOnly 订单 (`Order<Pending>`)。
+    ///
+    /// # 返回值
+    ///
     fn reject_post_only_order(&mut self, order: &Order<Pending>) -> Result<OrderRole, ExecutionError>
     {
         self.remove_order_from_pending_registry(order.client_order_id)?; // 移除订单
@@ -205,6 +375,17 @@ impl AccountOrders
                               order_role: role } }
     }
 
+
+    /// 增加请求计数器的值。
+    ///
+    /// 该函数使用 [`Ordering::Relaxed`] 来递增请求计数器 `request_counter` 的值，
+    /// 不保证线程同步的顺序一致性。这意味着多个线程可以并发调用此函数，
+    /// 但不保证对其他线程的立即可见性或顺序一致性。
+    ///
+    /// # 注意
+    ///
+    /// - 此函数的主要用途是在每次接收到新的订单请求时递增计数器，以确保订单 ID 的唯一性。
+    /// - 由于使用了 `Relaxed` 顺序，这种递增操作的结果可能对其他线程不可见。
     pub fn increment_request_counter(&self)
     {
         self.request_counter.fetch_add(1, Ordering::Relaxed);
