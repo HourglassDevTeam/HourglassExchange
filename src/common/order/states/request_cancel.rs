289f8a238d86dcacfa807e7b36bf9946d3aa9783
arthur <arthur@ciuyat.com>
2024-08-27 20:54:40 +0800

    rename common_infra.. to common

--- src/common/order/states/request_cancel.rs
+++ src/common/order/states/request_cancel.rs
@@ -1,14 +1,20 @@
 use crate::common::order::OrderId;
 use serde::{Deserialize, Serialize};
 
-/// 在RequestCancel结构体中只记录OrderId的原因主要是因为取消订单操作通常只需要知道哪个订单需要被取消。
+/// `RequestCancel` 结构体表示一个取消订单的请求。
+///
+/// 在 `RequestCancel` 结构体中只记录了 `OrderId`，原因是取消订单操作通常只需要知道
+/// 哪个订单需要被取消，无需其他额外信息。
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
 pub struct RequestCancel
 {
+    /// 要取消的订单的唯一标识符。
     pub id: OrderId,
 }
 
-// 从Id直接生成RequestCancel
+/// 通过 `OrderId` 或其他可以转换为 `OrderId` 的类型生成 `RequestCancel` 实例。
+///
+/// 这个实现允许从不同的标识符类型（如字符串）直接创建 `RequestCancel` 实例。
 impl<Id> From<Id> for RequestCancel where Id: Into<OrderId>
 {
     fn from(id: Id) -> Self
--- src/common/order/states/request_open.rs
+++ src/common/order/states/request_open.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize, Serialize};
 use std::cmp::Ordering;
 
+
 /// 订单初始状态。发送到client进行操作
+///
+/// `RequestOpen` 用于表示一个初始订单状态。这个状态包含了订单的价格、大小，以及是否为 `reduce_only` 订单。
 #[derive(Copy, Clone, PartialEq, Debug, Deserialize, Serialize)]
 pub struct RequestOpen
 {
@@ -10,22 +13,25 @@ pub struct RequestOpen
     pub size: f64,
 }
 
-/// FIXME this comparison is now only for the tests below but could be ill-logic.
+/// 通过实现 `PartialOrd`，我们可以基于 `RequestOpen` 的 `price`、`size` 和 `reduce_only` 字段进行排序和比较。
+/// 这种实现假定价格是最重要的排序标准，其次是订单大小，最后是 `reduce_only` 标志。
+///
+/// FIXME: 这种比较逻辑可能不符合实际需求，应根据具体业务需求进行调整。
 impl PartialOrd for RequestOpen
 {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering>
     {
-        // First compare by `price`
+        // 首先比较 `price`
         match self.price.partial_cmp(&other.price) {
-            | Some(Ordering::Equal) => {}
-            | non_eq => return non_eq,
+            Some(Ordering::Equal) => {},
+            non_eq => return non_eq,
         }
-        // Then compare by `size`
+        // 然后比较 `size`
         match self.size.partial_cmp(&other.size) {
-            | Some(Ordering::Equal) => {}
-            | non_eq => return non_eq,
+            Some(Ordering::Equal) => {},
+            non_eq => return non_eq,
         }
-        // Finally compare by `reduce_only`
+        // 最后比较 `reduce_only`
         Some(self.reduce_only.cmp(&other.reduce_only))
     }
 }
