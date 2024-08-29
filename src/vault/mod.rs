a2ae648df5a8de186b522e4626275c61642acdb8
arthur <arthur@ciuyat.com>
2024-08-29 19:49:47 +0800

    tweak Vault module

--- src/vault/mod.rs
+++ src/vault/mod.rs
@@ -1,45 +1,46 @@
 use crate::common::{balance::Balance, instrument::Instrument};
 use uuid::Uuid;
-// use crate::common::position::Position;
 use crate::{vault::error::VaultError, Exchange};
+use crate::common::position::Position;
+use crate::common::position::position_id::PositionId;
 
 pub mod error;
 pub mod in_memory;
 pub mod redis;
-// /// 处理 [`Position`] 在持久层的读写操作。
-// pub trait PositionHandler {
-//     /// 使用 [`PositionId`] 更新或插入一个打开的 [`Position`]。
-//     fn set_open_position(&mut self, position: Position) -> Result<(), VaultError>;
-//
-//     /// 使用提供的 [`PositionId`] 获取一个打开的 [`Position`]。
-//     fn get_open_position(
-//         &mut self,
-//         position_id: &PositionId,
-//     ) -> Result<Option<Position>, VaultError>;
-//
-//     /// 获取与一个投资组合相关联的所有打开的 [`Position`]。
-//     fn get_open_positions<'a, Markets: Iterator<Item = &'a Market>>(
-//         &mut self,
-//         engine_id: Uuid,
-//         markets: Markets,
-//     ) -> Result<Vec<Position>, VaultError>;
-//
-//     /// 移除在 [`PositionId`] 位置的 [`Position`]。
-//     fn remove_position(
-//         &mut self,
-//         position_id: &PositionId,
-//     ) -> Result<Option<Position>, VaultError>;
-//
-//     /// 将一个已退出的 [`Position`] 附加到投资组合的已退出持仓列表中。
-//     fn set_exited_position(
-//         &mut self,
-//         engine_id: Uuid,
-//         position: Position,
-//     ) -> Result<(), VaultError>;
-//
-//     /// 获取与 engine_id 相关联的所有已退出的 [`Position`]。
-//     fn get_exited_positions(&mut self, engine_id: Uuid) -> Result<Vec<Position>, VaultError>;
-// }
+/// 处理 [`Position`] 在持久层的读写操作。
+pub trait PositionHandler {
+    /// 使用 [`PositionId`] 更新或插入一个打开的 [`Position`]。
+    fn set_open_position(&mut self, position: Position) -> Result<(), VaultError>;
+
+    /// 使用提供的 [`PositionId`] 获取一个打开的 [`Position`]。
+    fn get_open_position(
+        &mut self,
+        position_id: &PositionId,
+    ) -> Result<Option<Position>, VaultError>;
+
+    /// 获取与一个投资组合相关联的所有打开的 [`Position`]。
+    fn get_open_positions(
+        &mut self,
+        engine_id: Uuid,
+        exchange: Exchange, instrument: Instrument
+    ) -> Result<Vec<Position>, VaultError>;
+
+    /// 移除在 [`PositionId`] 位置的 [`Position`]。
+    fn remove_position(
+        &mut self,
+        position_id: &PositionId,
+    ) -> Result<Option<Position>, VaultError>;
+
+    /// 将一个已退出的 [`Position`] 附加到投资组合的已退出持仓列表中。
+    fn set_exited_position(
+        &mut self,
+        engine_id: Uuid,
+        position: Position,
+    ) -> Result<(), VaultError>;
+
+    /// 获取与 engine_id 相关联的所有已退出的 [`Position`]。
+    fn get_exited_positions(&mut self, engine_id: Uuid) -> Result<Vec<Position>, VaultError>;
+}
 
 /// 处理投资组合当前余额在持久层的读写操作。
 pub trait BalanceHandler
@@ -50,10 +51,9 @@ pub trait BalanceHandler
     fn get_balance(&mut self, engine_id: Uuid) -> Result<Balance, VaultError>;
 }
 
-/// 处理投资组合中每个市场的统计数据的读写操作，每个市场由 [`MarketId`] 表示。
+/// 处理投资组合中每个市场的统计数据的读写操作。
 pub trait StatisticHandler<Statistic>
 {
-    /// 使用 [`MarketId`] 更新或插入市场统计数据。
     fn set_statistics(&mut self, exchange: Exchange, instrument: Instrument, statistic: Statistic) -> Result<(), VaultError>;
     fn get_statistics(&mut self, exchange: Exchange, instrument: Instrument) -> Result<Statistic, VaultError>;
 }
