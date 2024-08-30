61389f49e865b232f734a7cdd8e16a36bb687692
arthur <arthur@ciuyat.com>
2024-08-30 10:50:06 +0800

    add summary module

--- src/statistics/summary/data.rs
+++ src/statistics/summary/data.rs
@@ -1,162 +1,203 @@
-// use crate::statistic::{algorithm::welford_online, dispersion::Dispersion, summary::TableBuilder};
-// use prettytable::Row;
-// use serde::{Deserialize, Serialize};
-//
-// #[derive(Copy, Clone, PartialEq, PartialOrd, Debug, Default, Deserialize, Serialize)]
-// pub struct DataSummary {
-//     pub count: u64,
-//     pub sum: f64,
-//     pub mean: f64,
-//     pub dispersion: Dispersion,
-// }
-//
-// impl DataSummary {
-//     pub fn update(&mut self, next_value: f64) {
-//         // Increment counter
-//         self.count += 1;
-//
-//         // Update Sum
-//         self.sum += next_value;
-//
-//         // Update Mean
-//         let prev_mean = self.mean;
-//         self.mean = welford_online::calculate_mean(self.mean, next_value, self.count as f64);
-//
-//         // Update Dispersion
-//         self.dispersion
-//             .update(prev_mean, self.mean, next_value, self.count);
-//     }
-// }
-//
-// impl TableBuilder for DataSummary {
-//     fn titles(&self) -> Row {
-//         row![
-//             "Count",
-//             "Sum",
-//             "Mean",
-//             "Variance",
-//             "Std. Dev",
-//             "Range High",
-//             "Range Low",
-//         ]
-//     }
-//
-//     fn row(&self) -> Row {
-//         row![
-//             self.count,
-//             format!("{:.3}", self.sum),
-//             format!("{:.3}", self.mean),
-//             format!("{:.3}", self.dispersion.variance),
-//             format!("{:.3}", self.dispersion.std_dev),
-//             format!("{:.3}", self.dispersion.range.high),
-//             format!("{:.3}", self.dispersion.range.low),
-//         ]
-//     }
-// }
-//
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::statistic::dispersion::Range;
-//
-//     #[test]
-//     fn update_data_summary_with_position() {
-//         struct TestCase {
-//             input_next_value: f64,
-//             expected_summary: DataSummary,
-//         }
-//
-//         let mut data_summary = DataSummary::default();
-//
-//         let test_cases = vec![
-//             TestCase {
-//                 // Test case 0
-//                 input_next_value: 1.1,
-//                 expected_summary: DataSummary {
-//                     count: 1,
-//                     sum: 1.1,
-//                     mean: 1.1,
-//                     dispersion: Dispersion {
-//                         range: Range {
-//                             activated: true,
-//                             high: 1.1,
-//                             low: 1.1,
-//                         },
-//                         recurrence_relation_m: 0.00,
-//                         variance: 0.0,
-//                         std_dev: 0.0,
-//                     },
-//                 },
-//             },
-//             TestCase {
-//                 // Test case 1
-//                 input_next_value: 1.2,
-//                 expected_summary: DataSummary {
-//                     count: 2,
-//                     sum: 2.3,
-//                     mean: (2.3 / 2.0),
-//                     dispersion: Dispersion {
-//                         range: Range {
-//                             activated: true,
-//                             high: 1.2,
-//                             low: 1.1,
-//                         },
-//                         recurrence_relation_m: 0.005,
-//                         variance: 0.0025,
-//                         std_dev: 0.05,
-//                     },
-//                 },
-//             },
-//             TestCase {
-//                 // Test case 2
-//                 input_next_value: 1.3,
-//                 expected_summary: DataSummary {
-//                     count: 3,
-//                     sum: (2.3 + 1.3),
-//                     mean: (3.6 / 3.0),
-//                     dispersion: Dispersion {
-//                         range: Range {
-//                             activated: true,
-//                             high: 1.3,
-//                             low: 1.1,
-//                         },
-//                         recurrence_relation_m: 0.02,
-//                         variance: 1.0 / 150.0,
-//                         std_dev: (6.0_f64.sqrt() / 30.0),
-//                     },
-//                 },
-//             },
-//         ];
-//
-//         for (index, test) in test_cases.into_iter().enumerate() {
-//             data_summary.update(test.input_next_value);
-//             assert_eq!(
-//                 data_summary.count, test.expected_summary.count,
-//                 "Count Input: {:?}",
-//                 index
-//             );
-//             assert_eq!(
-//                 data_summary.sum, test.expected_summary.sum,
-//                 "Sum Input: {:?}",
-//                 index
-//             );
-//             assert_eq!(
-//                 data_summary.mean, test.expected_summary.mean,
-//                 "Mean Input: {:?}",
-//                 index
-//             );
-//
-//             let recurrence_diff = data_summary.dispersion.recurrence_relation_m
-//                 - test.expected_summary.dispersion.recurrence_relation_m;
-//             assert!(recurrence_diff < 1e-10, "Recurrence Input: {:?}", index);
-//
-//             let variance_diff =
-//                 data_summary.dispersion.variance - test.expected_summary.dispersion.variance;
-//             assert!(variance_diff < 1e-10, "Variance Input: {:?}", index);
-//
-//             let std_dev_diff =
-//                 data_summary.dispersion.std_dev - test.expected_summary.dispersion.std_dev;
-//             assert!(std_dev_diff < 1e-10, "Std. Dev. Input: {:?}", index);
-//         }
-//     }
-// }
+use prettytable::{row, Row};
+use serde::{Deserialize, Serialize};
+use crate::statistics::dispersion::Dispersion;
+use crate::statistics::summary::TableBuilder;
+use crate::statistics::welford_online;
+
+/// `DataSummary` 结构体用于汇总数据集的统计信息，包括计数、总和、均值以及离散度（方差、标准差和范围）。
+///
+/// # 原理介绍
+/// 数据汇总是一种常见的统计操作，它可以帮助我们从一组数据中提取出一些关键的统计信息，如数据的数量、总和、平均值，以及描述数据分散程度的方差、标准差和范围。
+///
+/// 该结构体结合了 Welford 在线算法和自定义的离散度（Dispersion）结构体，能够高效地在数据逐一输入的情况下动态更新这些统计量。
+///
+/// - **计数（Count）**：记录数据集中元素的数量。
+/// - **总和（Sum）**：所有数据点的累加和。
+/// - **均值（Mean）**：数据点的平均值。
+/// - **离散度（Dispersion）**：描述数据分散程度的度量，包括方差、标准差和范围。
+#[derive(Copy, Clone, PartialEq, PartialOrd, Debug, Default, Deserialize, Serialize)]
+pub struct DataSummary {
+    /// 数据点的数量
+    pub count: u64,
+    /// 数据点的总和
+    pub sum: f64,
+    /// 数据点的均值
+    pub mean: f64,
+    /// 数据点的离散度（方差、标准差和范围）
+    pub dispersion: Dispersion,
+}
+
+impl DataSummary {
+    /// 更新数据汇总结构体，给定一个新的数据点。
+    ///
+    /// # 原理解释
+    /// 当一个新的数据点被添加时，我们需要更新结构体中的计数、总和、均值以及离散度。
+    ///
+    /// 具体步骤如下：
+    /// 1. **计数增加**：每次添加新数据点，计数器都会增加。
+    /// 2. **更新总和**：将新数据点的值加入总和。
+    /// 3. **更新均值**：使用 Welford 在线算法高效地更新均值。
+    /// 4. **更新离散度**：基于新的均值和数据点，动态更新离散度（方差、标准差和范围）。
+    ///
+    /// # 参数
+    /// - `next_value`: 新的数据点的值
+    pub fn update(&mut self, next_value: f64) {
+        // 增加计数器
+        self.count += 1;
+        // 更新总和
+        self.sum += next_value;
+        // 更新均值
+        let prev_mean = self.mean;
+        self.mean = welford_online::update_mean(self.mean, next_value, self.count as f64);
+        // 更新离散度
+        self.dispersion
+            .update(prev_mean, self.mean, next_value, self.count);
+    }
+}
+
+/// 实现 `TableBuilder` 特性，用于生成表格表示形式的 `DataSummary`。
+///
+/// # 表格生成
+/// 该实现允许将 `DataSummary` 对象的统计信息以表格的形式展示出来，
+/// 包括计数、总和、均值、方差、标准差，以及范围的最高值和最低值。
+impl TableBuilder for DataSummary {
+    /// 返回表格的标题行。
+    ///
+    /// # 返回
+    /// 返回包含统计量名称的 `Row` 对象。
+    fn titles(&self) -> Row {
+        row![
+            "Count",
+            "Sum",
+            "Mean",
+            "Variance",
+            "Std. Dev",
+            "Range High",
+            "Range Low",
+        ]
+    }
+
+    /// 返回表格的一行数据。
+    ///
+    /// # 返回
+    /// 返回包含统计量值的 `Row` 对象。
+    fn row(&self) -> Row {
+        row![
+            self.count,
+            format!("{:.3}", self.sum),
+            format!("{:.3}", self.mean),
+            format!("{:.3}", self.dispersion.variance),
+            format!("{:.3}", self.dispersion.std_dev),
+            format!("{:.3}", self.dispersion.range.high),
+            format!("{:.3}", self.dispersion.range.low),
+        ]
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::statistics::dispersion::Range;
+
+    #[test]
+    fn update_data_summary_with_position() {
+        struct TestCase {
+            input_next_value: f64,
+            expected_summary: DataSummary,
+        }
+
+        let mut data_summary = DataSummary::default();
+
+        let test_cases = vec![
+            TestCase {
+                // Test case 0
+                input_next_value: 1.1,
+                expected_summary: DataSummary {
+                    count: 1,
+                    sum: 1.1,
+                    mean: 1.1,
+                    dispersion: Dispersion {
+                        range: Range {
+                            activated: true,
+                            high: 1.1,
+                            low: 1.1,
+                        },
+                        recurrence_relation_m: 0.00,
+                        variance: 0.0,
+                        std_dev: 0.0,
+                    },
+                },
+            },
+            TestCase {
+                // Test case 1
+                input_next_value: 1.2,
+                expected_summary: DataSummary {
+                    count: 2,
+                    sum: 2.3,
+                    mean: (2.3 / 2.0),
+                    dispersion: Dispersion {
+                        range: Range {
+                            activated: true,
+                            high: 1.2,
+                            low: 1.1,
+                        },
+                        recurrence_relation_m: 0.005,
+                        variance: 0.0025,
+                        std_dev: 0.05,
+                    },
+                },
+            },
+            TestCase {
+                // Test case 2
+                input_next_value: 1.3,
+                expected_summary: DataSummary {
+                    count: 3,
+                    sum: (2.3 + 1.3),
+                    mean: (3.6 / 3.0),
+                    dispersion: Dispersion {
+                        range: Range {
+                            activated: true,
+                            high: 1.3,
+                            low: 1.1,
+                        },
+                        recurrence_relation_m: 0.02,
+                        variance: 1.0 / 150.0,
+                        std_dev: (6.0_f64.sqrt() / 30.0),
+                    },
+                },
+            },
+        ];
+
+        for (index, test) in test_cases.into_iter().enumerate() {
+            data_summary.update(test.input_next_value);
+            assert_eq!(
+                data_summary.count, test.expected_summary.count,
+                "Count Input: {:?}",
+                index
+            );
+            assert_eq!(
+                data_summary.sum, test.expected_summary.sum,
+                "Sum Input: {:?}",
+                index
+            );
+            assert_eq!(
+                data_summary.mean, test.expected_summary.mean,
+                "Mean Input: {:?}",
+                index
+            );
+
+            let recurrence_diff = data_summary.dispersion.recurrence_relation_m
+                - test.expected_summary.dispersion.recurrence_relation_m;
+            assert!(recurrence_diff < 1e-10, "Recurrence Input: {:?}", index);
+
+            let variance_diff =
+                data_summary.dispersion.variance - test.expected_summary.dispersion.variance;
+            assert!(variance_diff < 1e-10, "Variance Input: {:?}", index);
+
+            let std_dev_diff =
+                data_summary.dispersion.std_dev - test.expected_summary.dispersion.std_dev;
+            assert!(std_dev_diff < 1e-10, "Std. Dev. Input: {:?}", index);
+        }
+    }
+}
--- src/statistics/summary/mod.rs
+++ src/statistics/summary/mod.rs
@@ -1,81 +1,125 @@
-// pub mod data;
-// pub mod drawdown;
-// pub mod pnl;
-// pub mod trading;
-//
-// use crate::portfolio::position::Position;
-// use prettytable::{Cell, Row, Table};
-//
-// pub trait Initialiser {
-//     type Config: Copy;
-//     fn init(config: Self::Config) -> Self;
-// }
-//
-// pub trait PositionSummariser: Copy {
-//     fn update(&mut self, position: &Position);
-//     fn generate_summary(&mut self, positions: &[Position]) {
-//         for position in positions.iter() {
-//             self.update(position)
-//         }
-//     }
-// }
-//
-// pub trait TableBuilder {
-//     fn titles(&self) -> Row;
-//     fn row(&self) -> Row;
-//     fn table(&self, id_cell: &str) -> Table {
-//         let mut table = Table::new();
-//
-//         let mut titles = self.titles();
-//         titles.insert_cell(0, Cell::new(""));
-//         table.set_titles(titles);
-//
-//         let mut row = self.row();
-//         row.insert_cell(0, Cell::new(id_cell));
-//         table.add_row(row);
-//
-//         table
-//     }
-//     fn table_with<T: TableBuilder>(&self, id_cell: &str, another: (T, &str)) -> Table {
-//         let mut table = Table::new();
-//
-//         let mut titles = self.titles();
-//         titles.insert_cell(0, Cell::new(""));
-//         table.set_titles(titles);
-//
-//         let mut first_row = self.row();
-//         first_row.insert_cell(0, Cell::new(id_cell));
-//         table.add_row(first_row);
-//
-//         let mut another_row = another.0.row();
-//         another_row.insert_cell(0, Cell::new(another.1));
-//         table.add_row(another_row);
-//
-//         table
-//     }
-// }
-//
-// pub fn combine<Iter, T>(builders: Iter) -> Table
-// where
-//     Iter: IntoIterator<Item = (String, T)>,
-//     T: TableBuilder,
-// {
-//     builders
-//         .into_iter()
-//         .enumerate()
-//         .fold(Table::new(), |mut table, (index, (id, builder))| {
-//             // Set Table titles using the first builder
-//             if index == 0 {
-//                 let mut titles = builder.titles();
-//                 titles.insert_cell(0, Cell::new(""));
-//                 table.set_titles(titles);
-//             }
-//
-//             // Add rows for each builder
-//             let mut row = builder.row();
-//             row.insert_cell(0, Cell::new(&id));
-//             table.add_row(row);
-//
-//             table
-//         })
-// }
+pub mod data;
+pub mod drawdown;
+pub mod pnl;
+pub mod trading;
+
+use prettytable::{Cell, Row, Table};
+use crate::common::position::Position;
+
+/// 该模块定义了一些用于处理交易数据和生成摘要表格的通用工具和接口。
+///
+/// # 原理介绍
+/// 这个模块的主要目的是提供一套接口和工具，用于处理交易数据，并生成可视化的摘要表格。这对于金融交易系统非常有用，可以帮助用户快速总结和查看交易的关键数据。
+///
+/// 具体来说：
+/// - `Initialiser` 特性（trait）定义了一个初始化器接口，它要求实现者能够通过配置初始化自身。
+/// - `PositionSummariser` 特性定义了一个更新和生成交易仓位摘要的接口。通过实现这个接口，可以在处理一组交易仓位时快速生成统计数据。
+/// - `TableBuilder` 特性定义了一套接口，用于生成表格标题、行数据以及完整表格。它还支持将多个表格合并为一个表格。
+/// - `combine` 函数用于合并多个表格生成器，生成一个包含所有行数据的完整表格。
+
+/// 用于初始化结构体的接口，要求实现者能够通过配置进行初始化。
+pub trait Initialiser {
+    type Config;
+    fn init(config: Self::Config) -> Self;
+}
+
+/// 用于生成交易仓位摘要的接口，提供更新仓位和生成仓位摘要的功能。
+pub trait PositionSummariser {
+    /// 更新当前仓位摘要，根据传入的 `Position` 对象更新内部状态。
+    fn update(&mut self, position: &Position);
+
+    /// 生成仓位摘要，根据一组 `Position` 对象迭代调用 `update` 方法。
+    fn generate_summary(&mut self, positions: &[Position]) {
+        for position in positions.iter() {
+            self.update(position)
+        }
+    }
+}
+
+/// 用于生成表格的接口，提供生成标题行、数据行和完整表格的功能。
+pub trait TableBuilder {
+    /// 返回表格的标题行。
+    fn titles(&self) -> Row;
+
+    /// 返回表格的一行数据。
+    fn row(&self) -> Row;
+
+    /// 生成带有 ID 列的完整表格。
+    ///
+    /// # 参数
+    /// - `id_cell`: 表格第一列的 ID 字符串
+    ///
+    /// # 返回
+    /// 返回包含标题和一行数据的表格。
+    fn table(&self, id_cell: &str) -> Table {
+        let mut table = Table::new();
+
+        let mut titles = self.titles();
+        titles.insert_cell(0, Cell::new(""));
+        table.set_titles(titles);
+
+        let mut row = self.row();
+        row.insert_cell(0, Cell::new(id_cell));
+        table.add_row(row);
+
+        table
+    }
+
+    /// 生成包含两个表格数据的完整表格，每个表格都有自己的 ID 列。
+    ///
+    /// # 参数
+    /// - `id_cell`: 第一个表格的 ID 字符串
+    /// - `another`: 另一个表格生成器及其对应的 ID 字符串
+    ///
+    /// # 返回
+    /// 返回包含两个表格数据的完整表格。
+    fn table_with<T: TableBuilder>(&self, id_cell: &str, another: (T, &str)) -> Table {
+        let mut table = Table::new();
+
+        let mut titles = self.titles();
+        titles.insert_cell(0, Cell::new(""));
+        table.set_titles(titles);
+
+        let mut first_row = self.row();
+        first_row.insert_cell(0, Cell::new(id_cell));
+        table.add_row(first_row);
+
+        let mut another_row = another.0.row();
+        another_row.insert_cell(0, Cell::new(another.1));
+        table.add_row(another_row);
+
+        table
+    }
+}
+
+/// 合并多个表格生成器，生成一个包含所有行数据的完整表格。
+///
+/// # 参数
+/// - `builders`: 表格生成器的迭代器，每个生成器都包含一个 ID 字符串和实现了 `TableBuilder` 特性的对象。
+///
+/// # 返回
+/// 返回包含所有行数据的合并表格。
+pub fn combine<Iter, T>(builders: Iter) -> Table
+where
+    Iter: IntoIterator<Item = (String, T)>,
+    T: TableBuilder,
+{
+    builders
+        .into_iter()
+        .enumerate()
+        .fold(Table::new(), |mut table, (index, (id, builder))| {
+            // 使用第一个生成器设置表格标题
+            if index == 0 {
+                let mut titles = builder.titles();
+                titles.insert_cell(0, Cell::new(""));
+                table.set_titles(titles);
+            }
+
+            // 为每个生成器添加行数据
+            let mut row = builder.row();
+            row.insert_cell(0, Cell::new(&id));
+            table.add_row(row);
+
+            table
+        })
+}
