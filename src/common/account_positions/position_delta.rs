260fd20b5dd60ed32cf94e662ac445c553df0337
arthur <arthur@ciuyat.com>
2024-09-07 16:26:22 +0800

    add PositionDelta for the exchange, note that size_delta could be either positive or negative

--- src/common/account_positions/position_delta.rs
+++ src/common/account_positions/position_delta.rs
@@ -12,11 +12,11 @@ use crate::common::Side;
 /// 该结构体在量化交易系统中至关重要，因为它能够实时跟踪仓位的变化，特别是在价格波动的情况下，帮助交易者和策略及时调整仓位。
 ///
 /// # 字段说明
-/// - `position_id`: [`Position`] 的唯一标识符，由交易所、交易符号以及进入时间生成，确保每个仓位都有一个唯一的 ID。
-/// - `update_time`: 更新事件的时间戳，记录该次更新发生的时间，通常表示市场事件发生时的时间。
-/// - `current_symbol_price`: 当前交易标的（symbol）的收盘价格，即市场上最新的价格。
-/// - `current_value_gross`: 当前仓位的总价值，计算方式为仓位的绝对数量乘以当前的市场价格（`abs(Quantity) * current_symbol_price`）。该字段不考虑费用或其他调整，仅反映仓位的粗略市值。
-/// - `unrealised_profit_loss`: 仓位未实现的盈亏，表示在仓位未平仓的情况下，基于当前市场价格计算的盈亏。这是一个浮动值，随着市场价格的变化而变化。
+/// - `side`: 仓位的方向，例如买入`Side::Buy`或卖出`Side::Sell`
+/// - `instrument`: 交易标的的详细信息，包括基础Token和报价Token
+/// - `update_time`: 仓位更新的时间戳，通常是一个 UNIX 时间戳，表示最后一次更新的时间
+/// - `size_delta`: 当前仓位变化量，通常以资产的数量表示
+/// - `current_symbol_price`: 当前交易标的的市场价格，可能是买入价或卖出价
 ///
 /// # 用途
 /// `PositionDelta` 主要用于实时更新仓位的状态，使得策略或交易者能够在市场变化时及时调整仓位或进行风控。
@@ -27,6 +27,6 @@ pub struct PositionDelta {
     pub side: Side,                         // 静态数据
     pub instrument: Instrument,             // 静态数据
     pub update_time: i64,                   // 实时数据
-    pub current_size:f64,                   // 实时数据
+    pub size_delta:f64,                   // 实时数据
     pub current_symbol_price: f64,          // 实时数据
 }
