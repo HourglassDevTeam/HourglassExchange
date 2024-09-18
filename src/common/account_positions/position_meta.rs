b54fdb34ecc16806c66adc5fcdb984869cec1997
arthur <arthur@ciuyat.com>
2024-09-18 14:22:51 +0800

    typo

--- src/common/account_positions/position_meta.rs
+++ src/common/account_positions/position_meta.rs
@@ -12,7 +12,7 @@ pub struct PositionMeta
     pub position_id: PositionId,      // 静态数据
     pub enter_ts: i64,                // 静态数据
     pub update_ts: i64,               // 实时更新
-    pub exit_balance: TokenBalance,   // 静态更新（退出时更新）当一个仓位被平仓（即完全退出）时，该仓位所涉及的资产或资金的最终状态。 CONSIDER  do we retrieve it from the TokenBalance or we calculate it?
+    pub exit_balance: TokenBalance,   // 静态更新（退出时更新）当一个仓位被平仓（即完全退出）时，该仓位所涉及的资产或资金的最终状态。 CONSIDER  do we retrieve it from the TokenBalance or simply calculate it?
     pub exchange: Exchange,           // 静态数据
     pub instrument: Instrument,       // 静态数据
     pub side: Side,                   // 静态数据
